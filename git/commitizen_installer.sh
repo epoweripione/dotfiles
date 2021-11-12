e3df3184238dd7037b4490da9200224fc89aef26
epoweripione <siyuwuxin@gmail.com>
2021-11-12 15:50:07 +0800

    refactor(commitflow): 🌀 make commit, lint & changelog work together

--- git/commitizen_installer.sh
+++ git/commitizen_installer.sh
@@ -71,65 +71,130 @@ fi
 # Install and run Commitizen locally
 if [[ "${IS_INSTALL}" == "yes" && -x "$(command -v npx)" ]]; then
     # commitizen
-    npx commitizen init cz-conventional-changelog --save-dev --save-exact && \
-        ./node_modules/.bin/commitizen init cz-conventional-changelog --save-dev --save-exact
+    npm install commitizen conventional-changelog-cli conventional-changelog-cz-emoji-config --save-dev && \
+        npx commitizen init cz-conventional-changelog --save-dev --save-exact
 
-    ## husky commitlint
-    # npm install husky @commitlint/config-conventional @commitlint/cli --save-dev && \
-    #     npx husky install && \
-    #     npm set-script prepare "husky install" && \
-    #     npx husky add .husky/commit-msg 'npx --no -- commitlint --edit "$1"' && \
-    #     npm set-script prepare-commit-msg "exec < /dev/tty && git cz --hook || true"
+    # husky & commitlint
+    npm install husky @commitlint/config-conventional @commitlint/cli --save-dev
 
     # cz-emoji: Commitizen adapter formatting commit messages using emojis
     # https://github.com/ngryman/cz-emoji
-    npm install conventional-changelog-cli cz-emoji commitlint commitlint-config-gitmoji --save-dev
+    npm install cz-emoji commitlint-config-gitmoji --save-dev
+fi
 
+## git hooks
+# if [[ -s "${CURRENT_DIR}/node_modules/.bin/cz" ]]; then
+#     if [[ ! -s "${CURRENT_DIR}/.git/hooks/prepare-commit-msg" ]]; then
+#         {
+#             echo '#!/bin/bash'
+#             echo 'exec < /dev/tty && node_modules/.bin/cz --hook || true'
+#         } >> "${CURRENT_DIR}/.git/hooks/prepare-commit-msg"
+#         chmod +x "${CURRENT_DIR}/.git/hooks/prepare-commit-msg"
+#     fi
+# fi
+
+# husky hooks
+if [[ -s "${CURRENT_DIR}/node_modules/.bin/husky" ]]; then
+    npx husky install && \
+        npm set-script prepare "husky install"
+fi
+
+if [[ ! -s "${CURRENT_DIR}/.husky/commit-msg" ]]; then
+    # npx husky add .husky/commit-msg 'npx --no -- commitlint --edit "$1"'
+    {
+        echo '#!/bin/sh'
+        echo '. "$(dirname "$0")/_/husky.sh"'
+        echo
+        echo 'npx --no -- commitlint --edit "$1"'
+    } > "${CURRENT_DIR}/.husky/commit-msg"
+fi
+chmod +x "${CURRENT_DIR}/.husky/commit-msg"
+
+if [[ ! -s "${CURRENT_DIR}/.husky/prepare-commit-msg" ]]; then
+    # npx husky add .husky/prepare-commit-msg 'exec < /dev/tty && node_modules/.bin/cz --hook || true'
+    {
+        echo '#!/bin/sh'
+        echo '. "$(dirname "$0")/_/husky.sh"'
+        echo
+        echo 'exec < /dev/tty && node_modules/.bin/cz --hook || true'
+    } > "${CURRENT_DIR}/.husky/prepare-commit-msg"
+fi
+chmod +x "${CURRENT_DIR}/.husky/prepare-commit-msg"
+
+# cz-emoji configuration
+if [[ ! -s "${CURRENT_DIR}/package.json" ]]; then
     cat "${CURRENT_DIR}/package.json" \
         | jq -r '."config"."commitizen"."path"="cz-emoji"' \
         | jq -r '."config"."cz-emoji"."symbol"=true' \
         | jq -r '."config"."cz-emoji"."conventional"=true' \
         | tee "${CURRENT_DIR}/package.json" >/dev/null
 
-    tee "${CURRENT_DIR}/commitlint.config.js" >/dev/null <<-'EOF'
-module.exports = {
-  extends: ['gitmoji'],
-  parserPreset: {
-    parserOpts: {
-      headerPattern: /^(:\w*:)(?:\s)(?:\((.*?)\))?\s((?:.*(?=\())|.*)(?:\(#(\d*)\))?/,
-      headerCorrespondence: ['type', 'scope', 'subject', 'ticket']
-    }
-  }
-}
-EOF
-
-    if ! grep -q "node_modules/" "${CURRENT_DIR}/.gitignore" 2>/dev/null; then
-        echo "node_modules/" >> "${CURRENT_DIR}/.gitignore"
-    fi
-
-    # if ! grep -q "package-lock.json" "${CURRENT_DIR}/.gitignore" 2>/dev/null; then
-    #     echo "package-lock.json" >> "${CURRENT_DIR}/.gitignore"
-    # fi
-
-    if [[ ! -s "${CURRENT_DIR}/.git/hooks/prepare-commit-msg" ]]; then
-        echo '#!/bin/bash' >> "${CURRENT_DIR}/.git/hooks/prepare-commit-msg"
-        echo 'exec < /dev/tty && node_modules/.bin/cz --hook || true' >> "${CURRENT_DIR}/.git/hooks/prepare-commit-msg"
-        chmod +x "${CURRENT_DIR}/.git/hooks/prepare-commit-msg"
-    fi
-
-    # gitmoji-cli
-    # https://github.com/carloscuesta/gitmoji-cli
-    [[ ! -x "$(command -v gitmoji)" ]] && npm install -g gitmoji-cli
-    [[ -x "$(command -v gitmoji)" ]] && noproxy_cmd gitmoji --list
-
-    # 🤖 🚀 ✨ Emojify your conventional commits with Devmoji
-    # https://github.com/folke/devmoji
-    [[ ! -x "$(command -v devmoji)" ]] && npm install -g devmoji
-    [[ -x "$(command -v devmoji)" ]] && devmoji --list
-    # echo "This is a :test: of the first :release: :boom: :sparkles:" | devmoji --format unicode
-    # echo "🚀 :boom: :sparkles:" | devmoji --format devmoji
-    # glol | devmoji --format unicode
+    # commit types
+    # cat "${CURRENT_DIR}/package.json" \
+    #     | jq -r '."config"."cz-emoji"."types"=."config"."cz-emoji"."types" + [{"emoji": "✨","code": ":sparkles:","description": "Introducing new features.","name": "feat"}]' \
+    #     | jq -r '."config"."cz-emoji"."types"=."config"."cz-emoji"."types" + [{"emoji": "🌀","code": ":cyclone:","description": "Refactoring code.","name": "refactor"}]' \
+    #     | tee "${CURRENT_DIR}/package.json" >/dev/null
 fi
 
+# commit types
+# https://gitmoji.dev/api/gitmojis
+if [[ ! -s "${CURRENT_DIR}/cz-emoji-types.json" ]]; then
+    curl "${CURL_CHECK_OPTS[@]}" -o "${CURRENT_DIR}/cz-emoji-types.json" \
+        "https://raw.githubusercontent.com/epoweripione/dotfiles/main/cz-emoji-types.json"
+
+    ## How to merge every two lines into one from the command line?
+    ## https://stackoverflow.com/questions/9605232/how-to-merge-every-two-lines-into-one-from-the-command-line
+    # jq -r '."config"."cz-emoji"."types"[] | .name, .emoji' cz-emoji-types.json \
+    #     | xargs -n2 -d'\n' \
+    #     | sed -e "s/^/'/" -e "s/$/',/" -e "s/ /': '/"
+fi
+
+if [[ -s "${CURRENT_DIR}/cz-emoji-types.json" ]]; then
+    CZ_EMOJI_TYPES=$(cat "${CURRENT_DIR}/cz-emoji-types.json" | jq -r '."config"."cz-emoji"."types"')
+    cat "${CURRENT_DIR}/package.json" \
+        | jq -r ".\"config\".\"cz-emoji\".\"types\"=${CZ_EMOJI_TYPES}" \
+        | tee "${CURRENT_DIR}/package.json" >/dev/null
+fi
+
+# commitlint configuration
+# headerPattern: /^(:\w*:)(?:\s)(?:\((.*?)\))?\s((?:.*(?=\())|.*)(?:\(#(\d*)\))?/,
+if [[ ! -s "${CURRENT_DIR}/commitlint.config.js" ]]; then
+    curl "${CURL_CHECK_OPTS[@]}" -o "${CURRENT_DIR}/commitlint.config.js" \
+        "https://raw.githubusercontent.com/epoweripione/dotfiles/main/commitlint.config.js"
+fi
+
+# git ignore
+if ! grep -q "node_modules/" "${CURRENT_DIR}/.gitignore" 2>/dev/null; then
+    echo "node_modules/" >> "${CURRENT_DIR}/.gitignore"
+fi
+
+# if ! grep -q "package-lock.json" "${CURRENT_DIR}/.gitignore" 2>/dev/null; then
+#     echo "package-lock.json" >> "${CURRENT_DIR}/.gitignore"
+# fi
+
+# gitmoji-cli
+# https://github.com/carloscuesta/gitmoji-cli
+[[ ! -x "$(command -v gitmoji)" ]] && npm install -g gitmoji-cli
+[[ -x "$(command -v gitmoji)" ]] && noproxy_cmd gitmoji --list
+
+## 🤖 🚀 ✨ Emojify your conventional commits with Devmoji
+## https://github.com/folke/devmoji
+# [[ ! -x "$(command -v devmoji)" ]] && npm install -g devmoji
+# [[ -x "$(command -v devmoji)" ]] && devmoji --list
+## echo "This is a :test: of the first :release: :boom: :sparkles:" | devmoji --format unicode
+## echo "🚀 :boom: :sparkles:" | devmoji --format devmoji
+## glol | devmoji --format unicode
+
+## check the last commit
+# npx commitlint --from HEAD~1 --to HEAD --verbose
+
 ## Generate a CHANGELOG from git metadata
+## Based on commits since the last semver tag that matches the pattern of "Feature", "Fix", "Performance Improvement" or "Breaking Changes"
 # ./node_modules/.bin/conventional-changelog -p angular -i CHANGELOG.md -s
+
+## Generate all previous changelogs
+# ./node_modules/.bin/conventional-changelog -p angular -i CHANGELOG.md -s -r 0
+
+## Generate changelogs with gitmoji
+# https://github.com/epoweripione/conventional-changelog-cz-emoji-config
+# ./node_modules/.bin/conventional-changelog -p cz-emoji-config -i CHANGELOG.md -s
