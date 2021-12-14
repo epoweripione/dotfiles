e732e18bbe144af0d7da94c0f2c827ad8cb9b91d
epoweripione <siyuwuxin@gmail.com>
2021-12-14 10:51:23 +0800

    feat(installer): ✨ add llama installer

---
+++ installer/llama_installer.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+trap 'rm -rf "${WORKDIR}"' EXIT
+
+[[ -z "${WORKDIR}" || ! -d "${WORKDIR}" ]] && WORKDIR="$(mktemp -d)"
+[[ -z "${CURRENT_DIR}" || ! -d "${CURRENT_DIR}" ]] && CURRENT_DIR=$(pwd)
+
+# Load custom functions
+if type 'colorEcho' 2>/dev/null | grep -q 'function'; then
+    :
+else
+    if [[ -s "${MY_SHELL_SCRIPTS:-$HOME/.dotfiles}/custom_functions.sh" ]]; then
+        source "${MY_SHELL_SCRIPTS:-$HOME/.dotfiles}/custom_functions.sh"
+    else
+        echo "${MY_SHELL_SCRIPTS:-$HOME/.dotfiles}/custom_functions.sh does not exist!"
+        exit 0
+    fi
+fi
+
+App_Installer_Reset
+
+# llama: Terminal file manager
+# https://github.com/antonmedv/llama
+APP_INSTALL_NAME="llama"
+GITHUB_REPO_NAME="antonmedv/llama"
+
+EXEC_INSTALL_NAME="llama"
+
+VERSION_FILENAME="${EXEC_INSTALL_PATH}/${EXEC_INSTALL_NAME}.version"
+
+if [[ -x "$(command -v ${EXEC_INSTALL_NAME})" ]]; then
+    IS_UPDATE="yes"
+    # CURRENT_VERSION=$(${EXEC_INSTALL_NAME} -V 2>&1 | grep -Eo '([0-9]{1,}\.)+[0-9]{1,}' | head -n1)
+    [[ -s "${VERSION_FILENAME}" ]] && CURRENT_VERSION=$(head -n1 "${VERSION_FILENAME}")
+    EXEC_FULL_NAME=$(which ${EXEC_INSTALL_NAME})
+else
+    [[ "${IS_UPDATE_ONLY}" == "yes" ]] && IS_INSTALL="no"
+fi
+
+REMOTE_URL="https://api.github.com/repos/${GITHUB_REPO_NAME}/releases/latest"
+if App_Installer_Get_Remote "${REMOTE_URL}" 'llama_.*' "llama_.*"; then
+    if App_Installer_Install; then
+        :
+    else
+        colorEcho "${RED}  Install ${FUCHSIA}${APP_INSTALL_NAME}${RED} failed!"
+    fi
+fi
+
+# export LLAMA_EDITOR=nano
+# function llcd() {
+#     llama "$@" 2> /tmp/path && cd "$(cat /tmp/path)" || return 0
+# }
+
+cd "${CURRENT_DIR}" || exit
