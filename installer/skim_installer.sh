81468a81e3f143d15bcd51d4b44a380256d0ea74
epoweripione <siyuwuxin@gmail.com>
2022-08-20 11:10:02 +0800

    feat(installer): ✨ add skim installer

---
+++ installer/skim_installer.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+trap 'rm -rf "${WORKDIR}"' EXIT
+
+[[ -z "${WORKDIR}" || "${WORKDIR}" != "/tmp/"* || ! -d "${WORKDIR}" ]] && WORKDIR="$(mktemp -d)"
+[[ -z "${CURRENT_DIR}" || ! -d "${CURRENT_DIR}" ]] && CURRENT_DIR=$(pwd)
+
+# Load custom functions
+if type 'colorEcho' 2>/dev/null | grep -q 'function'; then
+    :
+else
+    if [[ -s "${MY_SHELL_SCRIPTS:-$HOME/.dotfiles}/custom_functions.sh" ]]; then
+        source "${MY_SHELL_SCRIPTS:-$HOME/.dotfiles}/custom_functions.sh"
+    else
+        echo "${MY_SHELL_SCRIPTS:-$HOME/.dotfiles}/custom_functions.sh does not exist!"
+        exit 0
+    fi
+fi
+
+App_Installer_Reset
+
+# skim - Fuzzy Finder in rust
+# https://github.com/lotabout/skim
+APP_INSTALL_NAME="skim"
+GITHUB_REPO_NAME="lotabout/skim"
+
+EXEC_INSTALL_NAME="sk"
+
+ARCHIVE_EXT="tar.gz"
+ARCHIVE_EXEC_NAME="sk"
+
+if [[ -x "$(command -v ${EXEC_INSTALL_NAME})" ]]; then
+    IS_UPDATE="yes"
+    CURRENT_VERSION=$(${EXEC_INSTALL_NAME} --version 2>&1 | grep -Eo '([0-9]{1,}\.)+[0-9]{1,}' | head -n1)
+else
+    [[ "${IS_UPDATE_ONLY}" == "yes" ]] && IS_INSTALL="no"
+fi
+
+if ! App_Installer_Install; then
+    colorEcho "${RED}  Install ${FUCHSIA}${APP_INSTALL_NAME}${RED} failed!"
+fi
+
+cd "${CURRENT_DIR}" || exit
