0b32f8997c3b2d0f7a05540e18528256ff9f336d
epoweripione <siyuwuxin@gmail.com>
2022-08-26 10:07:44 +0800

    feat(installer): ✨ add notepadqq installer

---
+++ installer/notepadqq_installer.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+trap 'rm -rf "${WORKDIR}"' EXIT
+
+[[ -z "${WORKDIR}" || "${WORKDIR}" != "/tmp/"* || ! -d "${WORKDIR}" ]] && WORKDIR="$(mktemp -d)"
+[[ -z "${CURRENT_DIR}" || ! -d "${CURRENT_DIR}" ]] && CURRENT_DIR=$(pwd)
+
+# Load custom functions
+if type 'colorEcho' 2>/dev/null | grep -q 'function'; then
+    :
+else
+    if [[ -s "${MY_SHELL_SCRIPTS:-$HOME/.dotfiles}/custom_functions.sh" ]]; then
+        source "${MY_SHELL_SCRIPTS:-$HOME/.dotfiles}/custom_functions.sh"
+    else
+        echo "${MY_SHELL_SCRIPTS:-$HOME/.dotfiles}/custom_functions.sh does not exist!"
+        exit 0
+    fi
+fi
+
+# Notepadqq: https://github.com/notepadqq/notepadqq
+APP_INSTALL_NAME="notepadqq"
+EXEC_INSTALL_NAME="notepadqq"
+
+# yay --noconfirm --needed -S community/notepadqq
+# yay --noconfirm --needed -S aur/notepadqq-git
+
+# Pre-requisite packages
+PackagesList=(
+    desktop-file-utils
+    hicolor-icon-theme
+    qt5
+    uchardet
+)
+for TargetPackage in "${PackagesList[@]}"; do
+    if checkPackageNeedInstall "${TargetPackage}"; then
+        colorEcho "${BLUE}  Installing ${FUCHSIA}${TargetPackage}${BLUE}..."
+        sudo pacman --noconfirm -S "${TargetPackage}"
+    fi
+done
+
+colorEcho "${BLUE}  Installing ${FUCHSIA}${APP_INSTALL_NAME}${BLUE} from source..."
+[[ -z "${GIT_CLONE_OPTS[*]}" ]] && Get_Git_Clone_Options
+git clone "${GIT_CLONE_OPTS[@]}" --depth=1 --recursive "https://github.com/notepadqq/notepadqq" "${WORKDIR}/${APP_INSTALL_NAME}" && \
+    cd "${WORKDIR}/${APP_INSTALL_NAME}" && \
+    ./configure --prefix /usr >/dev/null && \
+    make >/dev/null && \
+    sudo make install >/dev/null
+
+
+cd "${CURRENT_DIR}" || exit
