354113581ea3d7b956aba880773e2c932d263553
epoweripione <siyuwuxin@gmail.com>
2022-09-30 11:39:09 +0800

    chore: 🎫 disable weather when login

--- zsh/zsh_custom_conf.sh
+++ zsh/zsh_custom_conf.sh
@@ -776,9 +776,9 @@ if [[ "${OS_INFO_WSL}" =~ "Microsoft" || "${OS_INFO_WSL}" =~ "microsoft" ]]; the
     # FAST_HIGHLIGHT[chroma-git]="chroma/-ogit.ch"
 
     # get local weather
-    get_weather_custom
-else
-    get_os_desktop && [[ -n "${OS_INFO_DESKTOP}" ]] && get_weather_custom
+#     get_weather_custom
+# else
+#     get_os_desktop && [[ -n "${OS_INFO_DESKTOP}" ]] && get_weather_custom
 fi
 
 # Autostart Tmux/screen Session On Remote System When Logging In Via SSH
