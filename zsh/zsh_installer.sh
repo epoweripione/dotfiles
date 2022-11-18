01b94d83ff4043177d60b30668e1531702982cc9
epoweripione <siyuwuxin@gmail.com>
2022-11-18 09:25:54 +0800

    feat: ✨ add bind9

--- zsh/zsh_installer.sh
+++ zsh/zsh_installer.sh
@@ -63,6 +63,7 @@ if [[ -x "$(command -v pacman)" ]]; then
         # autojump
         # autojump-zsh
         bind-utils
+        bind9-utils
         binutils
         build-essential
         connect-proxy
