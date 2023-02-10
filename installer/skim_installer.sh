381562f499f9226c7065cf4882d86f082a8dad6f
epoweripione <siyuwuxin@gmail.com>
2023-02-10 16:12:01 +0800

    feat: ✨ use cargo build

--- installer/skim_installer.sh
+++ installer/skim_installer.sh
@@ -29,15 +29,51 @@ EXEC_INSTALL_NAME="sk"
 ARCHIVE_EXT="tar.gz"
 ARCHIVE_EXEC_NAME="sk"
 
+INSTALL_FROM_SOURCE="no"
+EXEC_FULL_NAME=""
+
+CURRENT_VERSION="0.0.0"
+
 if [[ -x "$(command -v ${EXEC_INSTALL_NAME})" ]]; then
     IS_UPDATE="yes"
     CURRENT_VERSION=$(${EXEC_INSTALL_NAME} --version 2>&1 | grep -Eo '([0-9]{1,}\.)+[0-9]{1,}' | head -n1)
+    EXEC_FULL_NAME=$(which ${EXEC_INSTALL_NAME})
 else
     [[ "${IS_UPDATE_ONLY}" == "yes" ]] && IS_INSTALL="no"
 fi
 
-if ! App_Installer_Install; then
-    colorEcho "${RED}  Install ${FUCHSIA}${APP_INSTALL_NAME}${RED} failed!"
+# if ! App_Installer_Install; then
+#     colorEcho "${RED}  Install ${FUCHSIA}${APP_INSTALL_NAME}${RED} failed!"
+# fi
+
+if [[ "${IS_INSTALL}" == "yes" ]]; then
+    colorEcho "${BLUE}Checking latest version for ${FUCHSIA}${APP_INSTALL_NAME}${BLUE}..."
+
+    CHECK_URL="https://api.github.com/repos/${GITHUB_REPO_NAME}/releases/latest"
+    App_Installer_Get_Remote_Version "${CHECK_URL}"
+    if version_le "${REMOTE_VERSION}" "${CURRENT_VERSION}"; then
+        IS_INSTALL="no"
+    fi
+fi
+
+if [[ "${IS_INSTALL}" == "yes" ]]; then
+    if ! checkPackageInstalled "${APP_INSTALL_NAME}"; then
+        [[ -x "$(command -v cargo)" || -x "$(command -v brew)" ]] && INSTALL_FROM_SOURCE="yes"
+    fi
+fi
+
+if [[ "${INSTALL_FROM_SOURCE}" == "yes" ]]; then
+    colorEcho "${BLUE}  Installing ${FUCHSIA}${APP_INSTALL_NAME} ${YELLOW}${REMOTE_VERSION}${BLUE}..."
+    # From source on crates.io
+    [[ -x "$(command -v cargo)" ]] && cargo install "${APP_INSTALL_NAME}"
+
+    # Install via Homebrew
+    [[ ! -x "$(command -v cargo)" && -x "$(command -v brew)" ]] && brew install "sk"
+elif [[ "${INSTALL_FROM_SOURCE}" == "no" ]]; then
+    if checkPackageNeedInstall "${APP_INSTALL_NAME}"; then
+        colorEcho "${BLUE}  Installing ${FUCHSIA}${APP_INSTALL_NAME} ${YELLOW}${REMOTE_VERSION}${BLUE}..."
+        [[ -x "$(command -v pacman)" ]] && sudo pacman --noconfirm -S "${APP_INSTALL_NAME}"
+    fi
 fi
 
 cd "${CURRENT_DIR}" || exit
