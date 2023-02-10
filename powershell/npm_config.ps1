c750cc8a21e8690fc549077a974ef8f070887892
epoweripione <siyuwuxin@gmail.com>
2023-02-10 16:01:29 +0800

    fix: 🐛 remove invalid options

--- powershell/npm_config.ps1
+++ powershell/npm_config.ps1
@@ -19,30 +19,30 @@ if (($ConfigAction -eq "AUTO") -and $UseMirror) {
     Write-Host "Change npm registry to npmmirror.com..." -ForegroundColor Blue
     npm config set registry https://registry.npmmirror.com
 
-    npm config set disturl https://npmmirror.com/dist # node-gyp
-    npm config set sass_binary_site https://npmmirror.com/mirrors/node-sass # node-sass
-    npm config set electron_mirror https://npmmirror.com/mirrors/electron/ # electron
-    npm config set puppeteer_download_host https://npmmirror.com/mirrors # puppeteer
-    npm config set chromedriver_cdnurl https://npmmirror.com/mirrors/chromedriver # chromedriver
-    npm config set operadriver_cdnurl https://npmmirror.com/mirrors/operadriver # operadriver
-    npm config set phantomjs_cdnurl https://npmmirror.com/mirrors/phantomjs # phantomjs
-    npm config set selenium_cdnurl https://npmmirror.com/mirrors/selenium # selenium
-    npm config set node_inspector_cdnurl https://npmmirror.com/mirrors/node-inspector # node-inspector
+    # npm config set disturl https://npmmirror.com/dist # node-gyp
+    # npm config set sass_binary_site https://npmmirror.com/mirrors/node-sass # node-sass
+    # npm config set electron_mirror https://npmmirror.com/mirrors/electron/ # electron
+    # npm config set puppeteer_download_host https://npmmirror.com/mirrors # puppeteer
+    # npm config set chromedriver_cdnurl https://npmmirror.com/mirrors/chromedriver # chromedriver
+    # npm config set operadriver_cdnurl https://npmmirror.com/mirrors/operadriver # operadriver
+    # npm config set phantomjs_cdnurl https://npmmirror.com/mirrors/phantomjs # phantomjs
+    # npm config set selenium_cdnurl https://npmmirror.com/mirrors/selenium # selenium
+    # npm config set node_inspector_cdnurl https://npmmirror.com/mirrors/node-inspector # node-inspector
 }
 
 if ($ConfigAction -eq "RESET") {
     Write-Host "Reset npm registry (npmjs.org)..." -ForegroundColor Blue
     npm config set registry https://registry.npmjs.org/
 
-    npm config delete disturl
-    npm config delete sass_binary_site
-    npm config delete electron_mirror
-    npm config delete puppeteer_download_host
-    npm config delete chromedriver_cdnurl
-    npm config delete operadriver_cdnurl
-    npm config delete phantomjs_cdnurl
-    npm config delete selenium_cdnurl
-    npm config delete node_inspector_cdnurl
+    # npm config delete disturl
+    # npm config delete sass_binary_site
+    # npm config delete electron_mirror
+    # npm config delete puppeteer_download_host
+    # npm config delete chromedriver_cdnurl
+    # npm config delete operadriver_cdnurl
+    # npm config delete phantomjs_cdnurl
+    # npm config delete selenium_cdnurl
+    # npm config delete node_inspector_cdnurl
 }
 
 npm config list
