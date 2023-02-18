545809ef11ef64c1445169403ace8ccb8676db81
epoweripione <siyuwuxin@gmail.com>
2023-02-18 10:49:49 +0800

    fix: 🐛 fix FZF_DEFAULT_OPTS

--- fzf_config.sh
+++ fzf_config.sh
@@ -37,17 +37,24 @@ if [[ -x "$(command -v fd)" ]]; then
 fi
 
 # export FZF_DEFAULT_OPTS="--ansi --multi $(fzf_sizer_preview_window_settings)"
-FZF_DEFAULT_OPTS="
---ansi
---multi
---bind='ctrl-j:preview-page-down,ctrl-k:preview-page-up,alt-j:preview-bottom,alt-k:preview-top'
---bind='tab:toggle+down,btab:toggle+up,shift-tab:toggle+up,ctrl-a:toggle-all,ctrl-s:toggle-sort'
---bind='?:toggle-preview,ctrl-space:toggle-preview,alt-w:toggle-preview-wrap,enter:accept'
---layout='reverse'
---preview-window='right,70%'
---height='70%'
-"
-export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS"
+FZF_DEFAULT_OPTS='--ansi --multi'\
+' --bind="ctrl-j:preview-page-down,ctrl-k:preview-page-up,alt-j:preview-bottom,alt-k:preview-top"'\
+' --bind="tab:toggle+down,btab:toggle+up,shift-tab:toggle+up,ctrl-a:toggle-all,ctrl-s:toggle-sort"'\
+' --bind="?:toggle-preview,ctrl-space:toggle-preview,alt-w:toggle-preview-wrap,enter:accept"'\
+' --layout="reverse" --preview-window="right,70%" --height="70%"'
+# FZF_DEFAULT_OPTS=$(cat <<- EOF
+# --ansi
+# --multi
+# --bind="ctrl-j:preview-page-down,ctrl-k:preview-page-up,alt-j:preview-bottom,alt-k:preview-top"
+# --bind="tab:toggle+down,btab:toggle+up,shift-tab:toggle+up,ctrl-a:toggle-all,ctrl-s:toggle-sort"
+# --bind="?:toggle-preview,ctrl-space:toggle-preview,alt-w:toggle-preview-wrap,enter:accept"
+# --layout="reverse"
+# --preview-window="right,70%"
+# --height="70%"
+# EOF
+# )
+# FZF_DEFAULT_OPTS=$(tr '\n' ' ' <<<"${FZF_DEFAULT_OPTS}" | sed 's/^ *//g' | sed 's/ *$//g')
+export FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS}"
 
 # Utility tool for using git interactively
 # https://github.com/wfxr/forgit
