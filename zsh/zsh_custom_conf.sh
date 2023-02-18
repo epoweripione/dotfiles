8a111ee1e9dbce1d35542416b276e20893ea51b3
epoweripione <siyuwuxin@gmail.com>
2023-02-18 11:09:07 +0800

    feat: ✨ update for homebrew 4.0

--- zsh/zsh_custom_conf.sh
+++ zsh/zsh_custom_conf.sh
@@ -357,6 +357,7 @@ if [[ -x "/home/linuxbrew/.linuxbrew/bin/brew" ]]; then
         export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.ustc.edu.cn/brew.git"
         export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.ustc.edu.cn/homebrew-core.git"
         export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles"
+        export HOMEBREW_API_DOMAIN="https://mirrors.ustc.edu.cn/homebrew-bottles/api"
     fi
 fi
 
