c11b440c5a029aa482f71f9f23a54f6fe6c14630
epoweripione <siyuwuxin@gmail.com>
2023-05-27 12:05:37 +0800

    feat(installer): ✨ add devbox

---
+++ installer/devbox_installer.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+trap 'rm -rf "${WORKDIR}"' EXIT
+
+[[ -z "${WORKDIR}" || "${WORKDIR}" != "/tmp/"* || ! -d "${WORKDIR}" ]] && WORKDIR="$(mktemp -d)"
+[[ -z "${CURRENT_DIR}" || ! -d "${CURRENT_DIR}" ]] && CURRENT_DIR=$(pwd)
+
+# Load custom functions
+if type 'colorEcho' 2>/dev/null | grep -q 'function'; then
+    :
+else
+    if [[ -s "${MY_SHELL_SCRIPTS:-$HOME/.dotfiles}/custom_functions.sh" ]]; then
+        source "${MY_SHELL_SCRIPTS:-$HOME/.dotfiles}/custom_functions.sh"
+    else
+        echo "${MY_SHELL_SCRIPTS:-$HOME/.dotfiles}/custom_functions.sh does not exist!"
+        exit 0
+    fi
+fi
+
+App_Installer_Reset
+
+# [Devbox: Instant, easy, and predictable development environments](https://github.com/jetpack-io/devbox)
+curl -fsSL https://get.jetpack.io/devbox | bash
+
+# [The Nixpkgs Repository and the Devbox Search Index](https://github.com/NixOS/nixpkgs)
