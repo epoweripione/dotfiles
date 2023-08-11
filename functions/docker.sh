d6d2ab106f1e21a7d05e51609c8a3402c3d1b88e
epoweripione <siyuwuxin@gmail.com>
2023-08-11 17:13:45 +0800

    refactor: 🌀 rewrite functions for docker

--- functions/docker.sh
+++ functions/docker.sh
@@ -1,29 +1,150 @@
 #!/usr/bin/env bash
 
 # docker mirrors
-function Set_Docker_Mirrors() {
+## https://github.com/docker-practice/docker-registry-cn-mirror-test/actions
+#     sudo tee /etc/docker/daemon.json >/dev/null <<-'EOF'
+# {
+#     "registry-mirrors": [
+#         "https://dockerproxy.com",
+#         "https://docker.mirrors.sjtug.sjtu.edu.cn",
+#         "https://hub-mirror.c.163.com"
+#     ]
+# }
+# EOF
+
+## fix
+## curl: (35) error:1414D172:SSL routines:tls12_check_peer_sigalg:wrong signature type
+## https: SSLError(SSLError(1, '[SSL: WRONG_SIGNATURE_TYPE] wrong signature type (_ssl.c:1123)'))
+# sudo sed -i 's/CipherString = DEFAULT@SECLEVEL=2/CipherString = DEFAULT@SECLEVEL=1/' /etc/ssl/openssl.cnf
+
+## testing
+# https GET "https://mirror.baidubce.com/v2/library/nginx/tags/list"
+# ht GET "https://hub-mirror.c.163.com/v2/library/nginx/tags/list"
+# curl -ikL -X GET "https://docker.mirrors.sjtug.sjtu.edu.cn/v2/library/nginx/tags/list"
+# curl -ikL -X GET "https://docker.mirrors.sjtug.sjtu.edu.cn/v2/library/nginx/manifests/latest"
+
+# jq -r '."registry-mirrors"=."registry-mirrors" + ["https://hub-mirror.c.163.com"]' "/etc/docker/daemon.json" | sudo tee "/etc/docker/daemon_temp.json" >/dev/null
+# jq -r 'del(."registry-mirrors")' "/etc/docker/daemon.json" | sudo tee "/etc/docker/daemon_temp.json" >/dev/null
+function dockerSetMirrors() {
     local REGISTRY_MIRRORS=$1
+
+    if [[ ! -x "$(command -v docker)" ]]; then
+        colorEcho "${FUCHSIA}docker${RED} is not installed!"
+        return 1
+    fi
 
     if [[ ! -x "$(command -v jq)" ]]; then
         colorEcho "${FUCHSIA}jq${RED} is not installed!"
         return 1
     fi
 
-    [[ -z "${REGISTRY_MIRRORS}" ]] && \
-        REGISTRY_MIRRORS='"https://ustc-edu-cn.mirror.aliyuncs.com","https://docker.mirrors.sjtug.sjtu.edu.cn","https://mirror.baidubce.com","https://hub-mirror.c.163.com"'
+    if check_os_wsl; then
+        # colorEcho "Running in ${FUCHSIA}WSL${RED}, you must setting registry in Docker Desktop!"
+        return 0
+    fi
+
+    if [[ -z "${REGISTRY_MIRRORS}" ]]; then
+        [[ -z "${MIRROR_DOCKER_REGISTRY}" ]] && return 0
+        REGISTRY_MIRRORS=${MIRROR_DOCKER_REGISTRY}
+    fi
 
     colorEcho "${BLUE}Setting docker registry-mirrors to:"
     colorEcho "${FUCHSIA}  ${REGISTRY_MIRRORS}"
 
     [[ ! -s "/etc/docker/daemon.json" ]] && echo '{}' | sudo tee "/etc/docker/daemon.json" >/dev/null
 
-    cat "/etc/docker/daemon.json" \
-        | jq -r ".\"registry-mirrors\"=[${REGISTRY_MIRRORS}]" \
-        | sudo tee "/etc/docker/daemon.json" >/dev/null
+    jq -r ".\"registry-mirrors\"=[${REGISTRY_MIRRORS}]" "/etc/docker/daemon.json" \
+        | sudo tee "/etc/docker/daemon_temp.json" >/dev/null
 
-    colorEcho "${BLUE}Restarting docker service:"
+    [[ -f "/etc/docker/daemon_temp.json" ]] && sudo mv -f "/etc/docker/daemon_temp.json" "/etc/docker/daemon.json"
+
+    colorEcho "${BLUE}Restarting docker service..."
     sudo systemctl daemon-reload && sudo systemctl restart docker
 }
+
+# clear mirrors
+function dockerClearMirrors() {
+    if [[ -s "/etc/docker/daemon.json" ]]; then
+        jq -r 'del(."registry-mirrors")' "/etc/docker/daemon.json" | sudo tee "/etc/docker/daemon_temp.json" >/dev/null
+
+        [[ -f "/etc/docker/daemon_temp.json" ]] && sudo mv -f "/etc/docker/daemon_temp.json" "/etc/docker/daemon.json"
+        sudo systemctl daemon-reload && sudo systemctl restart docker
+    fi
+}
+
+# remove unuse mirrors
+function dockerRemoveUnuseMirrors() {
+    local UNUSE_MIRRORS REMOVE_MIRRORS Target
+
+    [[ ! -s "/etc/docker/daemon.json" ]] && return 0
+
+    if [[ -z "${UNUSE_MIRRORS[*]}" ]]; then
+        UNUSE_MIRRORS=(
+            '"https://docker.mirrors.ustc.edu.cn"'
+            '"https://ustc-edu-cn.mirror.aliyuncs.com"'
+            '"https://mirror.baidubce.com"'
+            '"https://mirror.ccs.tencentyun.com"'
+            '"https://dockerproxy.com"'
+        )
+    fi
+
+    REMOVE_MIRRORS=""
+    for Target in "${UNUSE_MIRRORS[@]}"; do
+        if grep -q "${Target}" "/etc/docker/daemon.json" 2>/dev/null; then
+            [[ -z "${REMOVE_MIRRORS}" ]] && REMOVE_MIRRORS="${Target}" || REMOVE_MIRRORS="${REMOVE_MIRRORS},${Target}"
+        fi
+    done
+
+    if [[ -n "${REMOVE_MIRRORS}" ]]; then
+        jq -r ".\"registry-mirrors\"=.\"registry-mirrors\" - [${REMOVE_MIRRORS}]" "/etc/docker/daemon.json" \
+            | sudo tee "/etc/docker/daemon_temp.json" >/dev/null
+
+        [[ -f "/etc/docker/daemon_temp.json" ]] && sudo mv -f "/etc/docker/daemon_temp.json" "/etc/docker/daemon.json"
+        sudo systemctl daemon-reload && sudo systemctl restart docker
+    fi
+}
+
+# docker proxy
+function dockerSetProxy() {
+    if [[ -n "${HTTP_PROXY}" && -n "${HTTPS_PROXY}" && -n "${NO_PROXY}" ]]; then
+        sudo mkdir -p "/etc/systemd/system/docker.service.d"
+        sudo tee "/etc/systemd/system/docker.service.d/99-proxy.conf" >/dev/null <<-EOF
+[Service]
+Environment="HTTP_PROXY=${HTTP_PROXY}"
+Environment="HTTPS_PROXY=${HTTPS_PROXY}"
+Environment="NO_PROXY=${NO_PROXY}"
+EOF
+        sudo systemctl daemon-reload && sudo systemctl restart docker
+        sudo systemctl show --property=Environment docker
+    fi
+}
+
+# container runtime proxy
+function dockerSetContainerProxy() {
+    if [[ -n "${HTTP_PROXY}" && -n "${HTTPS_PROXY}" && -n "${NO_PROXY}" ]]; then
+        mkdir -p "$HOME/.docker"
+        tee "$HOME/.docker/config.json" >/dev/null <<-EOF
+{
+    "proxies":
+    {
+        "default":
+        {
+            "httpProxy": "${HTTP_PROXY}",
+            "httpsProxy": "${HTTPS_PROXY}",
+            "noProxy": "${NO_PROXY}"
+        }
+    }
+}
+EOF
+    fi
+}
+
+## docker build proxy
+# docker build . \
+#     --build-arg "HTTP_PROXY=${HTTP_PROXY}" \
+#     --build-arg "HTTPS_PROXY=${HTTPS_PROXY}" \
+#     --build-arg "NO_PROXY=${NO_PROXY}" \
+#     -t your/image:tag
 
 # docker pull: Pull multiple images together
 # https://github.com/moby/moby/issues/16106
@@ -71,4 +192,26 @@ function dockerRemoveDangling() {
     #         docker rmi "${imageTag}"
     #     done <<<"${list}"
     # fi
+}
+
+# Relocating the Docker root directory
+# docker info -f '{{ .DockerRootDir}}' # /var/lib/docker
+function dockerRelocateRoot() {
+    local DATA_ROOT=$1
+
+    if [[ ! -x "$(command -v jq)" ]]; then
+        colorEcho "${FUCHSIA}jq${RED} is not installed!"
+        return 1
+    fi
+
+    [[ -z "${DATA_ROOT}" ]] && DATA_ROOT="/data/docker"
+    sudo mkdir -p "${DATA_ROOT}"
+    
+    [[ ! -s "/etc/docker/daemon.json" ]] && echo '{}' | sudo tee "/etc/docker/daemon.json" >/dev/null
+
+    jq -r ".\"data-root\"=[${DATA_ROOT}]" "/etc/docker/daemon.json" \
+        | sudo tee "/etc/docker/daemon_temp.json" >/dev/null
+
+    [[ -f "/etc/docker/daemon_temp.json" ]] && sudo mv -f "/etc/docker/daemon_temp.json" "/etc/docker/daemon.json"
+    sudo systemctl daemon-reload && sudo systemctl restart docker
 }
