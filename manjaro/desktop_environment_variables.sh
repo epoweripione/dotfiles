ca8400ec5ea00eae6ed7f4ba0ade669cf34de2a0
epoweripione <siyuwuxin@gmail.com>
2023-12-25 09:30:11 +0800

    feat: ✨ add script for loading environment variables on login to DE

---
+++ manjaro/desktop_environment_variables.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# [Environment variables](https://wiki.archlinux.org/title/environment_variables)
+# [Session Environment Variables](https://userbase.kde.org/Session_Environment_Variables)
+if [[ -s "$HOME/.dotfiles/custom_functions.sh" ]]; then
+    source "$HOME/.dotfiles/custom_functions.sh"
+fi
