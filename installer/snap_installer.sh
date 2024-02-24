607a9299f4b0f10dc05c5399b8497424c1b5640c
epoweripione <siyuwuxin@gmail.com>
2024-02-24 15:13:21 +0800

    fix: 🐛 sudo systemctl

--- installer/snap_installer.sh
+++ installer/snap_installer.sh
@@ -41,7 +41,7 @@ fi
 if [[ -x "$(command -v snap)" ]]; then
     colorEcho "${BLUE}Setting ${FUCHSIA}snap${BLUE}..."
     systemctl is-enabled snapd.socket >/dev/null 2>&1 || sudo systemctl enable --now snapd.socket
-    systemctl is-enabled snapd.apparmor >/dev/null 2>&1 || systemctl enable --now snapd.apparmor
+    systemctl is-enabled snapd.apparmor >/dev/null 2>&1 || sudo systemctl enable --now snapd.apparmor
 
     # enable classic snap support
     [[ ! -d "/snap" && -d "/var/lib/snapd/snap" ]] && sudo ln -s /var/lib/snapd/snap /snap
