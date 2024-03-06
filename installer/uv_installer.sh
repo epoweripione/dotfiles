0c1a40b35c1fe4c2714d9bf104608904d09602d0
epoweripione <siyuwuxin@gmail.com>
2024-03-06 16:32:11 +0800

    feat: ✨ add uv

---
+++ installer/uv_installer.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+trap 'rm -rf "${WORKDIR}"' EXIT
+
+[[ -z "${WORKDIR}" || "${WORKDIR}" != "/tmp/"* || ! -d "${WORKDIR}" ]] && WORKDIR="$(mktemp -d)"
+[[ -z "${CURRENT_DIR}" || ! -d "${CURRENT_DIR}" ]] && CURRENT_DIR=$(pwd)
+
+# Load custom functions
+if type 'colorEcho' 2>/dev/null | grep -q 'function'; then
+    :
+else
+    if [[ -s "${MY_SHELL_SCRIPTS:-$HOME/.dotfiles}/custom_functions.sh" ]]; then
+        source "${MY_SHELL_SCRIPTS:-$HOME/.dotfiles}/custom_functions.sh"
+    else
+        echo "${MY_SHELL_SCRIPTS:-$HOME/.dotfiles}/custom_functions.sh does not exist!"
+        exit 0
+    fi
+fi
+
+App_Installer_Reset
+
+# [An extremely fast Python package installer and resolver, written in Rust](https://github.com/astral-sh/uv)
+INSTALLER_APP_NAME="uv"
+INSTALLER_GITHUB_REPO="astral-sh/uv"
+INSTALLER_BINARY_NAME="uv"
+
+if [[ -x "$(command -v ${INSTALLER_BINARY_NAME})" ]]; then
+    INSTALLER_IS_UPDATE="yes"
+    App_Installer_Get_Installed_Version "${INSTALLER_APP_NAME}"
+else
+    [[ "${IS_UPDATE_ONLY}" == "yes" ]] && INSTALLER_IS_INSTALL="no"
+fi
+
+if [[ "${INSTALLER_IS_INSTALL}" == "yes" ]]; then
+    colorEcho "${BLUE}Checking latest version for ${FUCHSIA}${INSTALLER_APP_NAME}${BLUE}..."
+    if App_Installer_Get_Remote_Version; then
+        if version_le "${INSTALLER_VER_REMOTE}" "${INSTALLER_VER_CURRENT}"; then
+            INSTALLER_IS_INSTALL="no"
+        fi
+    fi
+fi
+
+if [[ "${INSTALLER_IS_INSTALL}" == "yes" ]]; then
+    curl -LsSf https://astral.sh/uv/install.sh | sh
+fi
