6ae149b2e28c2d873c2605a7d0fc09138d1a2ef5
epoweripione <siyuwuxin@gmail.com>
2024-07-30 16:17:43 +0800

    refactor: 🌀 use installBuildBinary function to intall

--- installer/procs_installer.sh
+++ installer/procs_installer.sh
@@ -33,8 +33,6 @@ else
     [[ "${IS_UPDATE_ONLY}" == "yes" ]] && INSTALLER_IS_INSTALL="no"
 fi
 
-[[ ! -x "$(command -v cargo)" && ! -x "$(command -v brew)" ]] && INSTALLER_IS_INSTALL="no"
-
 if [[ "${INSTALLER_IS_INSTALL}" == "yes" ]]; then
     colorEcho "${BLUE}Checking latest version for ${FUCHSIA}${INSTALLER_APP_NAME}${BLUE}..."
 
@@ -48,17 +46,7 @@ fi
 # Install Latest Version
 if [[ "${INSTALLER_IS_INSTALL}" == "yes" ]]; then
     colorEcho "${BLUE}  Installing ${FUCHSIA}${INSTALLER_APP_NAME} ${YELLOW}${INSTALLER_VER_REMOTE}${BLUE}..."
-    # Install via Homebrew
-    if [[ -x "$(command -v brew)" ]]; then
-        if [[ -x "$(command -v ${INSTALLER_INSTALL_NAME})" ]]; then
-            brew upgrade "${INSTALLER_APP_NAME}"
-        else
-            brew install "${INSTALLER_APP_NAME}"
-        fi
-    fi
-
-    # From source on crates.io
-    [[ ! -x "$(command -v brew)" && -x "$(command -v cargo)" ]] && cargo install "${INSTALLER_APP_NAME}"
+    installBuildBinary "${INSTALLER_APP_NAME}" "${INSTALLER_INSTALL_NAME}" cargo
 fi
 
 
