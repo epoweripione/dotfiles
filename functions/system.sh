8eb58ddc23b496c537ccc1af83774da03b523e34
epoweripione <siyuwuxin@gmail.com>
2024-12-22 21:53:34 +0800

    fix: 🐛 get only number for snapper list

--- functions/system.sh
+++ functions/system.sh
@@ -247,8 +247,8 @@ function SnapperDeleteSnapshots() {
 
     keep_snapshot=$((keep_snapshot + 1))
     while read -r current_config; do
-        start_id=$(sudo snapper -c "${current_config}" list | awk '{print $1}' | grep -E '[[:digit:]]+' | grep -wv '0' | head -n1)
-        end_id=$(sudo snapper -c "${current_config}" list | awk '{print $1}' | grep -E '[[:digit:]]+'| grep -wv '0' | tail -n "${keep_snapshot}" | head -n1)
+        start_id=$(sudo snapper -c "${current_config}" list | awk '{print $1}' | grep -Eo '[[:digit:]]+' | grep -wv '0' | head -n1)
+        end_id=$(sudo snapper -c "${current_config}" list | awk '{print $1}' | grep -Eo '[[:digit:]]+'| grep -wv '0' | tail -n "${keep_snapshot}" | head -n1)
         if [[ -n "${start_id}" && -n "${end_id}" ]]; then
             [[ start_id -lt end_id ]] && sudo snapper -c "${current_config}" delete --sync "${start_id}-${end_id}"
         fi
