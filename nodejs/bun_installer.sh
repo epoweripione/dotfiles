de04ffa04dd90e916b2c75e08f69958add963c92
epoweripione <siyuwuxin@gmail.com>
2025-03-28 11:07:37 +0800

    feat: ✨ add bun

---
+++ nodejs/bun_installer.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Load custom functions
+if type 'colorEcho' 2>/dev/null | grep -q 'function'; then
+    :
+else
+    if [[ -s "${MY_SHELL_SCRIPTS:-$HOME/.dotfiles}/custom_functions.sh" ]]; then
+        source "${MY_SHELL_SCRIPTS:-$HOME/.dotfiles}/custom_functions.sh"
+    else
+        echo "${MY_SHELL_SCRIPTS:-$HOME/.dotfiles}/custom_functions.sh does not exist!"
+        exit 0
+    fi
+fi
+
+App_Installer_Reset
+
+# [Bun - a fast JavaScript bundler](https://bun.sh/)
+INSTALLER_GITHUB_REPO="oven-sh/bun"
+INSTALLER_BINARY_NAME="bun"
+
+if [[ -x "$(command -v ${INSTALLER_BINARY_NAME})" ]]; then
+    INSTALLER_IS_UPDATE="yes"
+    App_Installer_Get_Installed_Version "${INSTALLER_BINARY_NAME}"
+else
+    [[ "${IS_UPDATE_ONLY}" == "yes" ]] && INSTALLER_IS_INSTALL="no"
+fi
+
+if [[ "${INSTALLER_IS_INSTALL}" == "yes" ]]; then
+    colorEcho "${BLUE}Checking latest version for ${FUCHSIA}${INSTALLER_BINARY_NAME}${BLUE}..."
+
+    INSTALLER_CHECK_URL="https://api.github.com/repos/${INSTALLER_GITHUB_REPO}/releases/latest"
+    App_Installer_Get_Remote_Version "${INSTALLER_CHECK_URL}"
+    if version_le "${INSTALLER_VER_REMOTE}" "${INSTALLER_VER_CURRENT}"; then
+        INSTALLER_IS_INSTALL="no"
+    fi
+fi
+
+if [[ "${INSTALLER_IS_INSTALL}" == "yes" ]]; then
+    colorEcho "${BLUE}  Installing ${FUCHSIA}${INSTALLER_BINARY_NAME} ${YELLOW}${INSTALLER_VER_REMOTE}${BLUE}..."
+    if [[ "${INSTALLER_IS_UPDATE}" == "yes" ]]; then
+        bun upgrade
+    else
+        curl -fsSL https://bun.sh/install | bash
+    fi
+fi
