82b8da320fa97a5c52480b2d31aefef47110ae73
epoweripione <siyuwuxin@gmail.com>
2025-04-18 12:15:44 +0800

    feat: ✨ installer for manual install apps

---
+++ manjaro/manual_install_apps.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+trap 'rm -rf "${WORKDIR}"' EXIT
+
+[[ -z "${WORKDIR}" || "${WORKDIR}" != "/tmp/"* || ! -d "${WORKDIR}" ]] && WORKDIR="$(mktemp -d)"
+[[ -z "${CURRENT_DIR}" || ! -d "${CURRENT_DIR}" ]] && CURRENT_DIR=$(pwd)
+
+# Load custom functions
+if type 'colorEcho' 2>/dev/null | grep -q 'function'; then
+    :
+else
+    if [[ -s "${MY_SHELL_SCRIPTS:-$HOME/.dotfiles}/custom_functions.sh" ]]; then
+        source "${MY_SHELL_SCRIPTS:-$HOME/.dotfiles}/custom_functions.sh"
+    else
+        echo "${MY_SHELL_SCRIPTS:-$HOME/.dotfiles}/custom_functions.sh does not exist!"
+        exit 0
+    fi
+fi
+
+App_Installer_Reset
+
+MANUAL_INSTALL_APP_STORE="${1:-$HOME}"
+MANUAL_INSTALL_DIR="${2:-/opt}"
+
+DESKTOP_DIR=$(xdg-user-dir DESKTOP)
+
+[[ ! -d "${MANUAL_INSTALL_DIR}" ]] && mkdir -p "${MANUAL_INSTALL_DIR}"
+
+# [GoldenDict++OCR](https://autoptr.top/gdocr/GoldenDict-OCR-Deployment/)
+INSTALLER_ARCHIVE_FILENAME=$(find "${MANUAL_INSTALL_APP_STORE}" -type f -iname "goldendict*.tar.gz" -print0 | sort -r | head -n1)
+if [[ -f "${INSTALLER_ARCHIVE_FILENAME}" ]]; then
+    [[ -d "${MANUAL_INSTALL_DIR}/GoldenDict++OCR" ]] && sudo rm -rf "${MANUAL_INSTALL_DIR}/GoldenDict++OCR" || true
+
+    sudo tar -xzf "${INSTALLER_ARCHIVE_FILENAME}" -C "${MANUAL_INSTALL_DIR}" && \
+        sudo mv "${MANUAL_INSTALL_DIR}/GoldenDict" "${MANUAL_INSTALL_DIR}/GoldenDict++OCR" && \
+        sudo chown -R "$(id -u)":"$(id -g)" "${MANUAL_INSTALL_DIR}/GoldenDict++OCR" && \
+        sudo find "${MANUAL_INSTALL_DIR}/GoldenDict++OCR" -type d -print0 | xargs -0 chmod 755 && \
+        sudo chmod +x "${MANUAL_INSTALL_DIR}/GoldenDict++OCR/goldendict.sh" && \
+        sudo chmod +x "${MANUAL_INSTALL_DIR}/GoldenDict++OCR/GoldenDict"
+fi
+
+if [[ -f "${MANUAL_INSTALL_DIR}/GoldenDict++OCR/splash.png" && ! -f "/usr/share/pixmaps/GoldenDict++OCR.png" ]]; then
+    sudo cp "${MANUAL_INSTALL_DIR}/GoldenDict++OCR/splash.png" "/usr/share/pixmaps/GoldenDict++OCR.png"
+fi
+
+if [[ -f "${MANUAL_INSTALL_DIR}/GoldenDict++OCR/goldendict.sh" && ! -f "${DESKTOP_DIR}/GoldenDict++OCR.desktop" ]]; then
+    tee "${DESKTOP_DIR}/GoldenDict++OCR.desktop" >/dev/null <<-EOF
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=GoldenDict++OCR
+Comment=GoldenDict++OCR
+Exec=${MANUAL_INSTALL_DIR}/GoldenDict++OCR/goldendict.sh
+Icon=GoldenDict++OCR
+Path=${MANUAL_INSTALL_DIR}/GoldenDict++OCR
+Terminal=false
+StartupNotify=true
+GenericName=GoldenDict++OCR
+EOF
+fi
+
+## Run `GoldenDict++OCR`
+# if [[ -f "${MANUAL_INSTALL_DIR}/GoldenDict++OCR/goldendict.sh" ]]; then
+#     cd "${MANUAL_INSTALL_DIR}/GoldenDict++OCR" && ./goldendict.sh
+# fi
+
+
+# [xDroid](https://www.linzhuotech.com/Product/download)
+INSTALLER_ARCHIVE_FILENAME=$(find "${MANUAL_INSTALL_APP_STORE}" -type f -iname 'xdroid*.tar.xz' -print0 | sort -r | head -n1)
+INSTALLER_FILENAME=""
+if [[ -f "${INSTALLER_ARCHIVE_FILENAME}" ]]; then
+    if sudo tar -xJf "${INSTALLER_ARCHIVE_FILENAME}" -C "${MANUAL_INSTALL_DIR}"; then
+        INSTALLER_FILENAME=$(find "${MANUAL_INSTALL_DIR}" -type f -iname 'xdroid*.run' -print0 | sort -r | head -n1)
+    fi
+fi
+
+if [[ -n "${INSTALLER_FILENAME}" && -f "${INSTALLER_FILENAME}" ]]; then
+    "${INSTALLER_FILENAME}"
+fi
