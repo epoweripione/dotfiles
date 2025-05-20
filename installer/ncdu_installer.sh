bbc92fc53922cb808530727192565ad494654ec4
epoweripione <siyuwuxin@gmail.com>
2025-05-20 16:13:15 +0800

    refactor: 🌀 compile for zig version

--- installer/ncdu_installer.sh
+++ installer/ncdu_installer.sh
@@ -19,28 +19,74 @@ fi
 
 App_Installer_Reset
 
-# NCurses Disk Usage
-# https://dev.yorhel.nl/ncdu
+# [NCurses Disk Usage](https://dev.yorhel.nl/ncdu)
 INSTALLER_APP_NAME="ncdu"
-INSTALLER_GITHUB_REPO=""
-
-INSTALLER_INSTALL_PATH="/usr/local/bin"
 INSTALLER_INSTALL_NAME="ncdu"
-
 INSTALLER_ARCHIVE_EXT="tar.gz"
 
 if [[ -x "$(command -v ${INSTALLER_INSTALL_NAME})" ]]; then
     INSTALLER_IS_UPDATE="yes"
-    INSTALLER_VER_CURRENT=$(${INSTALLER_INSTALL_NAME} -V 2>&1 | grep -Eo '([0-9]{1,}\.)+[0-9]{1,}' | head -n1)
-    INSTALLER_EXEC_FULLNAME=$(readlink -f "$(which ${INSTALLER_INSTALL_NAME})")
+    App_Installer_Get_Installed_Version "${INSTALLER_INSTALL_NAME}"
 else
     [[ "${IS_UPDATE_ONLY}" == "yes" ]] && INSTALLER_IS_INSTALL="no"
 fi
 
-if App_Installer_Get_Remote_URL "https://dev.yorhel.nl/ncdu" 'ncdu-[^<>:;,?"*|/]+\.tar\.gz' "ncdu-.*\.tar\.gz"; then
-    INSTALLER_DOWNLOAD_URL="https://dev.yorhel.nl/download/${INSTALLER_DOWNLOAD_URL}"
-    if ! App_Installer_Install "https://dev.yorhel.nl/ncdu"; then
-        colorEcho "${RED}  Install ${FUCHSIA}${INSTALLER_APP_NAME}${RED} failed!"
+if [[ "${INSTALLER_IS_INSTALL}" == "yes" ]]; then
+    colorEcho "${BLUE}Checking ${FUCHSIA}${INSTALLER_APP_NAME}${BLUE}..."
+    INSTALLER_CHECK_URL="https://dev.yorhel.nl/ncdu"
+    App_Installer_Get_Remote_Version "${INSTALLER_CHECK_URL}" "ncdu-.*\.tar\.gz"
+    if version_le "${INSTALLER_VER_REMOTE}" "${INSTALLER_VER_CURRENT}"; then
+        INSTALLER_IS_INSTALL="no"
+    fi
+fi
+
+if [[ "${INSTALLER_IS_INSTALL}" == "yes" ]]; then
+    if App_Installer_Get_Remote_URL "${INSTALLER_CHECK_URL}" "ncdu-${INSTALLER_VER_REMOTE//./\\.}\.tar\.gz" "ncdu-.*\.tar\.gz"; then
+        INSTALLER_DOWNLOAD_URL="https://dev.yorhel.nl/download/${INSTALLER_DOWNLOAD_URL}"
+        if App_Installer_Download_Extract "${INSTALLER_DOWNLOAD_URL}" "${WORKDIR}/${INSTALLER_APP_NAME}.tar.gz" "${WORKDIR}"; then
+            mv "${WORKDIR}/${INSTALLER_APP_NAME}-"* "${WORKDIR}/${INSTALLER_APP_NAME}"
+            [[ -f "${WORKDIR}/${INSTALLER_APP_NAME}/build.zig" ]] && INSTALLER_INSTALL_METHOD="build"
+        fi
+    fi
+fi
+
+if [[ "${INSTALLER_INSTALL_METHOD}" == "build" ]]; then
+    if [[ ! -x "$(command -v zig)" && "$(command -v mise)" ]]; then
+        mise install zig
+        mise use --global zig
+    fi
+
+    if [[ -x "$(command -v zig)" ]]; then
+        # Pre-requisite packages
+        PackagesList=(
+            "libncurses-dev"
+            "libncursesw-dev"
+            "libncurses5-dev"
+            "libncursesw5-dev"
+            "libzstd"
+            "libzstd-dev"
+            "libzstd-devel"
+            "ncurses"
+            "ncurses-devel"
+        )
+        InstallSystemPackages "" "${PackagesList[@]}"
+
+        colorEcho "${BLUE}  Compiling ${FUCHSIA}${INSTALLER_APP_NAME}${BLUE}..."
+        cd "${WORKDIR}/${INSTALLER_APP_NAME}" && \
+            make && \
+            sudo install -m0755 zig-out/bin/ncdu "${INSTALLER_INSTALL_PATH}" &&
+            sudo install -m0644 ncdu.1 "${INSTALLER_MANPAGE_PATH}/man1"
+    else
+        INSTALLER_INSTALL_METHOD="no"
+    fi
+fi
+
+if [[ "${INSTALLER_IS_INSTALL}" == "yes" && "${INSTALLER_INSTALL_METHOD}" != "build" ]]; then
+    if App_Installer_Get_Remote_URL "${INSTALLER_CHECK_URL}" 'ncdu-[^<>:;,?"*|/]+\.tar\.gz' "ncdu-.*\.tar\.gz"; then
+        INSTALLER_DOWNLOAD_URL="https://dev.yorhel.nl/download/${INSTALLER_DOWNLOAD_URL}"
+        if ! App_Installer_Install "${INSTALLER_CHECK_URL}"; then
+            colorEcho "${RED}  Install ${FUCHSIA}${INSTALLER_APP_NAME}${RED} failed!"
+        fi
     fi
 fi
 
