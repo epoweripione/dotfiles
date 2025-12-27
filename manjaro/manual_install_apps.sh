97611578a8fcad75469820b6fdfc01ddd9f604c2
epoweripione <siyuwuxin@gmail.com>
2025-12-27 22:22:12 +0800

    refactor: 🌀 goldendictOCR++

--- manjaro/manual_install_apps.sh
+++ manjaro/manual_install_apps.sh
@@ -27,33 +27,49 @@ DESKTOP_DIR=$(xdg-user-dir DESKTOP)
 [[ ! -d "${MANUAL_INSTALL_DIR}" ]] && mkdir -p "${MANUAL_INSTALL_DIR}"
 
 # [GoldenDict++OCR](https://autoptr.top/gdocr/GoldenDict-OCR-Deployment/)
-INSTALLER_ARCHIVE_FILENAME="$(find "${MANUAL_INSTALL_APP_STORE}" -maxdepth 1 -type f -iname "goldendict*.tar.gz" | sort -r | head -n1)"
+INSTALLER_ARCHIVE_FILENAME="$(find "${MANUAL_INSTALL_APP_STORE}" -maxdepth 1 -type f -iname "goldendict*debian.tar.gz" | sort -r | head -n1)"
 if [[ -f "${INSTALLER_ARCHIVE_FILENAME}" ]]; then
     colorEcho "${BLUE}Installing ${FUCHSIA}GoldenDict++OCR${BLUE}..."
     [[ -d "${MANUAL_INSTALL_DIR}/GoldenDict++OCR" ]] && sudo rm -rf "${MANUAL_INSTALL_DIR}/GoldenDict++OCR" || true
 
     sudo tar -xzf "${INSTALLER_ARCHIVE_FILENAME}" -C "${MANUAL_INSTALL_DIR}" && \
-        sudo mv "${MANUAL_INSTALL_DIR}/GoldenDict" "${MANUAL_INSTALL_DIR}/GoldenDict++OCR" && \
-        sudo chown -R "$(id -u)":"$(id -g)" "${MANUAL_INSTALL_DIR}/GoldenDict++OCR" && \
-        sudo find "${MANUAL_INSTALL_DIR}/GoldenDict++OCR" -type d -print0 | xargs -0 chmod 755 && \
-        sudo chmod +x "${MANUAL_INSTALL_DIR}/GoldenDict++OCR/goldendict.sh" && \
-        sudo chmod +x "${MANUAL_INSTALL_DIR}/GoldenDict++OCR/GoldenDict"
+        sudo find "${MANUAL_INSTALL_DIR}/GoldenDict" -type d -print0 | xargs -0 sudo chmod 755
 fi
 
-if [[ -f "${MANUAL_INSTALL_DIR}/GoldenDict++OCR/splash.png" && ! -f "/usr/share/pixmaps/GoldenDict++OCR.png" ]]; then
-    sudo cp "${MANUAL_INSTALL_DIR}/GoldenDict++OCR/splash.png" "/usr/share/pixmaps/GoldenDict++OCR.png"
+if ! sudo test -d "${MANUAL_INSTALL_DIR}/GoldenDict/tessdata"; then
+    if [[ -f "${MANUAL_INSTALL_APP_STORE}/GoldenDict++OCR-Model-Files-onnx+ncnn+tess.tar.gz" ]]; then
+        colorEcho "${BLUE}Installing ${FUCHSIA}GoldenDict++OCR Model Files${BLUE}..."
+        sudo tar -xzf "${MANUAL_INSTALL_APP_STORE}/GoldenDict++OCR-Model-Files-onnx+ncnn+tess.tar.gz" -C "${MANUAL_INSTALL_DIR}"
+    fi
 fi
 
-if [[ -f "${MANUAL_INSTALL_DIR}/GoldenDict++OCR/goldendict.sh" && ! -f "${DESKTOP_DIR}/GoldenDict++OCR.desktop" ]]; then
+if ! sudo test -d "${MANUAL_INSTALL_DIR}/GoldenDict/nsocr"; then
+    if [[ -f "${MANUAL_INSTALL_APP_STORE}/GoldenDict++OCR-Model-Files-Linux_only.tar.gz" ]]; then
+        sudo tar -xzf "${MANUAL_INSTALL_APP_STORE}/GoldenDict++OCR-Model-Files-Linux_only.tar.gz" -C "${MANUAL_INSTALL_DIR}"
+    fi
+fi
+
+if sudo test -d "${MANUAL_INSTALL_DIR}/GoldenDict"; then
+    sudo chown -R "$(id -u)":"$(id -g)" "${MANUAL_INSTALL_DIR}/GoldenDict" && \
+        sudo find "${MANUAL_INSTALL_DIR}/GoldenDict" -type d -print0 | xargs -0 sudo chmod 755 && \
+        sudo chmod +x "${MANUAL_INSTALL_DIR}/GoldenDict/goldendict.sh" && \
+        sudo chmod +x "${MANUAL_INSTALL_DIR}/GoldenDict/GoldenDict"
+fi
+
+if [[ -f "${MANUAL_INSTALL_DIR}/GoldenDict/splash.png" && ! -f "/usr/share/pixmaps/GoldenDict++OCR.png" ]]; then
+    sudo cp "${MANUAL_INSTALL_DIR}/GoldenDict/splash.png" "/usr/share/pixmaps/GoldenDict++OCR.png"
+fi
+
+if [[ -f "${MANUAL_INSTALL_DIR}/GoldenDict/goldendict.sh" && ! -f "${DESKTOP_DIR}/GoldenDict++OCR.desktop" ]]; then
     tee "${DESKTOP_DIR}/GoldenDict++OCR.desktop" >/dev/null <<-EOF
 [Desktop Entry]
 Version=1.0
 Type=Application
 Name=GoldenDict++OCR
 Comment=GoldenDict++OCR
-Exec=${MANUAL_INSTALL_DIR}/GoldenDict++OCR/goldendict.sh
+Exec=${MANUAL_INSTALL_DIR}/GoldenDict/goldendict.sh
 Icon=GoldenDict++OCR
-Path=${MANUAL_INSTALL_DIR}/GoldenDict++OCR
+Path=${MANUAL_INSTALL_DIR}/GoldenDict
 Terminal=false
 StartupNotify=true
 GenericName=GoldenDict++OCR
@@ -61,7 +77,7 @@ EOF
 fi
 
 ## Run `GoldenDict++OCR`
-# if [[ -f "${MANUAL_INSTALL_DIR}/GoldenDict++OCR/goldendict.sh" ]]; then
+# if [[ -f "${MANUAL_INSTALL_DIR}/GoldenDict/goldendict.sh" ]]; then
 #     cd "${MANUAL_INSTALL_DIR}/GoldenDict++OCR" && ./goldendict.sh
 # fi
 
