8ed5d94bc19febc013fe2d5bd85d3f9f7aa97c1e
epoweripione <siyuwuxin@gmail.com>
2026-04-09 21:18:44 +0800

    refactor: 🌀 rewritten

--- powershell/pwsh_script_download.ps1
+++ powershell/pwsh_script_download.ps1
@@ -51,82 +51,90 @@ if (-Not (check_webservice_up)) {
 }
 
 
-Set-Location ~
+Set-Location -Path "$HOME"
 
 Write-Host "Downloading custom powershell scripts..." -ForegroundColor Blue
 $DOWNLOAD_URL = "https://github.com/epoweripione/dotfiles/archive/refs/heads/main.zip"
 if (($null -eq $SOCKS_PROXY_ADDR) -or ($SOCKS_PROXY_ADDR -eq "")) {
-    curl -fsL -o ".\dotfiles.zip" "$DOWNLOAD_URL"
+    curl -fsL -o "$HOME\dotfiles.zip" "$DOWNLOAD_URL"
 } else {
-    curl -fsL --socks5-hostname "$SOCKS_PROXY_ADDR" -o ".\dotfiles.zip" "$DOWNLOAD_URL"
+    curl -fsL --socks5-hostname "$SOCKS_PROXY_ADDR" -o "$HOME\dotfiles.zip" "$DOWNLOAD_URL"
 }
 
 if ($?) {
     Write-Host "Extracting script files..." -ForegroundColor Blue
-    Expand-Archive -Path ".\dotfiles.zip" -DestinationPath .
-    Rename-Item -Path ".\dotfiles-main" -NewName ".\dotfiles"
+    Expand-Archive -Path "$HOME\dotfiles.zip" -DestinationPath "$HOME" -Force
+    if (Test-Path "$HOME\dotfiles-main") {
+        if (Test-Path "$HOME\.dotfiles") {
+            Copy-Item -Path "$HOME\dotfiles-main\*" -Destination "$HOME\.dotfiles" -Recurse -Force -Confirm:$false
+        } else {
+            Rename-Item -Path "$HOME\dotfiles-main" -NewName "$HOME\.dotfiles"
+        }
+    }
 
     # Conda
-    if (-Not (Test-Path "~\.condarc")) {
-        Copy-Item -Path ".\dotfiles\conf\condarc" -Destination "~\.condarc"
+    if (-Not (Test-Path "$HOME\.condarc")) {
+        Copy-Item -Path "$HOME\.dotfiles\conf\condarc" -Destination "$HOME\.condarc"
     }
 
     # PIP
-    if (-Not (Test-Path "~\.pip")) {
-        Copy-Item -Path ".\dotfiles\conf\pip" -Destination "~\.pip"
+    if (-Not (Test-Path "$HOME\.pip")) {
+        Copy-Item -Path "$HOME\.dotfiles\conf\pip" -Destination "$HOME\.pip"
     }
 
     # Rust Cargo
-    if (-Not (Test-Path "~\.cargo")) {
-        New-Item -path "~\.cargo" -type Directory | Out-Null
+    if (-Not (Test-Path "$HOME\.cargo")) {
+        New-Item -path "$HOME\.cargo" -type Directory | Out-Null
     }
-    if (-Not (Test-Path "~\.cargo\config.toml")) {
-        Copy-Item -Path ".\dotfiles\conf\cargo.toml" -Destination "~\.cargo\config.toml"
+    if (-Not (Test-Path "$HOME\.cargo\config.toml")) {
+        Copy-Item -Path "$HOME\.dotfiles\conf\cargo.toml" -Destination "$HOME\.cargo\config.toml"
 
-        (Get-Content "~\.cargo\config.toml").Replace("crates-io-sparse","rsproxy-sparse").Replace("# replace-with","replace-with") | Set-Content "~\.cargo\config.toml"
+        (Get-Content "$HOME\.cargo\config.toml").Replace("crates-io-sparse","rsproxy-sparse").Replace("# replace-with","replace-with") | Set-Content "$HOME\.cargo\config.toml"
     }
 
     if (Test-Path "$env:CARGO_HOME") {
         if (-Not (Test-Path "$env:CARGO_HOME\config.toml")) {
-            Copy-Item -Path ".\dotfiles\conf\cargo.toml" -Destination "$env:CARGO_HOME\config.toml"
+            Copy-Item -Path "$HOME\.dotfiles\conf\cargo.toml" -Destination "$env:CARGO_HOME\config.toml"
 
             (Get-Content "$env:CARGO_HOME\config.toml").Replace("crates-io-sparse","rsproxy-sparse").Replace("# replace-with","replace-with") | Set-Content "$env:CARGO_HOME\config.toml"
         }
     }
 
     # Custom scripts
-    $PWSH_DIR = "~\Documents\PowerShell\Scripts"
-    if (-Not (Test-Path "$PWSH_DIR")) {New-Item -path "$PWSH_DIR" -type Directory | Out-Null}
-    # Copy-Item -Path ".\dotfiles\powershell\*" -Destination "$PWSH_DIR" -Recurse -Force -Confirm:$false
-    Copy-Item -Path ".\dotfiles\powershell\*.ps1" -Destination "$PWSH_DIR"
-    Copy-Item -Path ".\dotfiles\wsl\*.ps1" -Destination "$PWSH_DIR"
-    Copy-Item -Path ".\dotfiles\wsl\windows_terminal_profile.jsonc" -Destination "$PWSH_DIR"
-    Copy-Item -Path ".\dotfiles\cross\hosts_accelerate_cn.list" -Destination "$PWSH_DIR"
+    $PWSH_DIR = "$HOME\Documents\PowerShell\Scripts"
+    if (-Not (Test-Path "$PWSH_DIR")) {
+        New-Item -path "$PWSH_DIR" -type Directory | Out-Null
+    }
+    # Copy-Item -Path "$HOME\.dotfiles\powershell\*" -Destination "$PWSH_DIR" -Recurse -Force -Confirm:$false
+    Copy-Item -Path "$HOME\.dotfiles\powershell\*.ps1" -Destination "$PWSH_DIR"
+    Copy-Item -Path "$HOME\.dotfiles\wsl\*.ps1" -Destination "$PWSH_DIR"
+    Copy-Item -Path "$HOME\.dotfiles\wsl\windows_terminal_profile.jsonc" -Destination "$PWSH_DIR"
+    Copy-Item -Path "$HOME\.dotfiles\cross\hosts_accelerate_cn.list" -Destination "$PWSH_DIR"
 
     # PowerShell themes
-    $CONFIG_DIR = "~\.config"
+    $CONFIG_DIR = "$HOME\.config"
     if (-Not (Test-Path "$CONFIG_DIR")) {
         New-Item -path "$CONFIG_DIR" -type Directory | Out-Null
     }
-    Copy-Item -Path ".\dotfiles\powershell\themes\starship.toml" -Destination "$CONFIG_DIR"
+    Copy-Item -Path "$HOME\.dotfiles\powershell\themes\starship.toml" -Destination "$CONFIG_DIR"
 
-    $THEME_DIR = "~\Documents\PowerShell\PoshThemes"
+    $THEME_DIR = "$HOME\Documents\PowerShell\PoshThemes"
     if (-Not (Test-Path "$THEME_DIR")) {New-Item -path "$THEME_DIR" -type Directory | Out-Null}
-    Copy-Item -Path ".\dotfiles\powershell\themes\*.psm1" -Destination "$THEME_DIR"
-    Copy-Item -Path ".\dotfiles\powershell\themes\*.json" -Destination "$THEME_DIR"
+    Copy-Item -Path "$HOME\.dotfiles\powershell\themes\*.psm1" -Destination "$THEME_DIR"
+    Copy-Item -Path "$HOME\.dotfiles\powershell\themes\*.json" -Destination "$THEME_DIR"
 
-    # (Get-Content -path "~\Documents\PowerShell\PoshThemes\powerlevel10k_my.omp.json" -Raw) `
+    # (Get-Content -path "$HOME\Documents\PowerShell\PoshThemes\powerlevel10k_my.omp.json" -Raw) `
     #     -Replace '"type": "git",','"type": "poshgit",' `
-    #     | Set-Content -Path "~\Documents\PowerShell\PoshThemes\powerlevel10k_my.omp.json"
+    #     | Set-Content -Path "$HOME\Documents\PowerShell\PoshThemes\powerlevel10k_my.omp.json"
 
     # WSL background images
-    $IMAGE_DIR = "~\Pictures"
-    Copy-Item -Path ".\dotfiles\wsl\*.jpg" -Destination "$IMAGE_DIR"
+    $IMAGE_DIR = "$HOME\Pictures"
+    Copy-Item -Path "$HOME\.dotfiles\wsl\*.jpg" -Destination "$IMAGE_DIR"
 
     # Clean up
     Write-Host "Cleaning up..." -ForegroundColor Blue
-    Remove-Item -Path ".\dotfiles" -Recurse -Force -Confirm:$false
-    Remove-Item -Path ".\dotfiles.zip" -Force -Confirm:$false
+    # Remove-Item -Path "$HOME\dotfiles" -Recurse -Force -Confirm:$false
+    Remove-Item -Path "$HOME\dotfiles.zip" -Force -Confirm:$false
 } else {
     Write-Host "Download custom powershell scripts failed!" -ForegroundColor Red
 }
