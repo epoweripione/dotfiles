4cbce1420e5645db6a77c1543511596cf20bbd6d
epoweripione <siyuwuxin@gmail.com>
2026-04-15 10:38:40 +0800

    refactor: 🌀 remove unuse options

--- powershell/npm_config.ps1
+++ powershell/npm_config.ps1
@@ -17,7 +17,7 @@ if (check_webservice_up) {
 
 if (($ConfigAction -eq "AUTO") -and $UseMirror) {
     Write-Host "Change npm registry to npmmirror.com..." -ForegroundColor Blue
-    npm config set registry https://registry.npmmirror.com
+    npm config set registry "https://registry.npmmirror.com"
 
     # Add-Content "$env:USERPROFILE\.npmrc" "`ndisturl=https://npmmirror.com/dist" # node-gyp
     # Add-Content "$env:USERPROFILE\.npmrc" "`nsass_binary_site=https://npmmirror.com/mirrors/node-sass" # node-sass
@@ -31,84 +31,84 @@ if (($ConfigAction -eq "AUTO") -and $UseMirror) {
 
     [System.Environment]::SetEnvironmentVariable("FNM_NODE_DIST_MIRROR", 'https://cdn.npmmirror.com/binaries/node', 'User')
 
-    [System.Environment]::SetEnvironmentVariable("CHROMEDRIVER_CDNURL", 'https://cdn.npmmirror.com/binaries/chromedriver', 'User')
-    [System.Environment]::SetEnvironmentVariable("COREPACK_NPM_REGISTRY", 'https://registry.npmmirror.com', 'User')
-    [System.Environment]::SetEnvironmentVariable("CYPRESS_DOWNLOAD_PATH_TEMPLATE", 'https://cdn.npmmirror.com/binaries/cypress/${version}/${platform}-${arch}/cypress.zip', 'User')
-    [System.Environment]::SetEnvironmentVariable("EDGEDRIVER_CDNURL", 'https://npmmirror.com/mirrors/edgedriver', 'User')
-    [System.Environment]::SetEnvironmentVariable("ELECTRON_BUILDER_BINARIES_MIRROR", 'https://cdn.npmmirror.com/binaries/electron-builder-binaries/', 'User')
-    [System.Environment]::SetEnvironmentVariable("ELECTRON_MIRROR", 'https://cdn.npmmirror.com/binaries/electron/', 'User')
-    [System.Environment]::SetEnvironmentVariable("NODEJS_ORG_MIRROR", 'https://cdn.npmmirror.com/binaries/node', 'User')
-    [System.Environment]::SetEnvironmentVariable("NVM_NODEJS_ORG_MIRROR", 'https://cdn.npmmirror.com/binaries/node', 'User')
-    [System.Environment]::SetEnvironmentVariable("NWJS_URLBASE", 'https://cdn.npmmirror.com/binaries/nwjs/v', 'User')
-    [System.Environment]::SetEnvironmentVariable("OPERADRIVER_CDNURL", 'https://cdn.npmmirror.com/binaries/operadriver', 'User')
-    [System.Environment]::SetEnvironmentVariable("PHANTOMJS_CDNURL", 'https://cdn.npmmirror.com/binaries/phantomjs', 'User')
-    [System.Environment]::SetEnvironmentVariable("PLAYWRIGHT_DOWNLOAD_HOST", 'https://cdn.npmmirror.com/binaries/playwright', 'User')
-    [System.Environment]::SetEnvironmentVariable("PRISMA_ENGINES_MIRROR", 'https://cdn.npmmirror.com/binaries/prisma', 'User')
-    [System.Environment]::SetEnvironmentVariable("PUPPETEER_CHROME_DOWNLOAD_BASE_URL", 'https://cdn.npmmirror.com/binaries/chrome-for-testing', 'User')
-    [System.Environment]::SetEnvironmentVariable("PUPPETEER_CHROME_HEADLESS_SHELL_DOWNLOAD_BASE_URL", 'https://cdn.npmmirror.com/binaries/chrome-for-testing', 'User')
-    [System.Environment]::SetEnvironmentVariable("PUPPETEER_DOWNLOAD_BASE_URL", 'https://cdn.npmmirror.com/binaries/chrome-for-testing', 'User')
-    [System.Environment]::SetEnvironmentVariable("PUPPETEER_DOWNLOAD_HOST", 'https://cdn.npmmirror.com/binaries/chrome-for-testing', 'User')
-    [System.Environment]::SetEnvironmentVariable("RE2_DOWNLOAD_MIRROR", 'https://cdn.npmmirror.com/binaries/node-re2', 'User')
-    [System.Environment]::SetEnvironmentVariable("RE2_DOWNLOAD_SKIP_PATH", 'true', 'User')
-    [System.Environment]::SetEnvironmentVariable("SASS_BINARY_SITE", 'https://cdn.npmmirror.com/binaries/node-sass', 'User')
-    [System.Environment]::SetEnvironmentVariable("SAUCECTL_INSTALL_BINARY_MIRROR", 'https://cdn.npmmirror.com/binaries/saucectl', 'User')
-    [System.Environment]::SetEnvironmentVariable("SENTRYCLI_CDNURL", 'https://cdn.npmmirror.com/binaries/sentry-cli', 'User')
-    [System.Environment]::SetEnvironmentVariable("SWC_BINARY_SITE", 'https://cdn.npmmirror.com/binaries/node-swc', 'User')
-    # binary_host
-    [System.Environment]::SetEnvironmentVariable("better_sqlite3_binary_host", 'https://cdn.npmmirror.com/binaries/better-sqlite3', 'User')
-    [System.Environment]::SetEnvironmentVariable("gl_binary_host", 'https://cdn.npmmirror.com/binaries/gl', 'User')
-    [System.Environment]::SetEnvironmentVariable("keytar_binary_host", 'https://cdn.npmmirror.com/binaries/keytar', 'User')
-    [System.Environment]::SetEnvironmentVariable("robotjs_binary_host", 'https://cdn.npmmirror.com/binaries/robotjs', 'User')
-    [System.Environment]::SetEnvironmentVariable("sharp_binary_host", 'https://cdn.npmmirror.com/binaries/sharp', 'User')
-    [System.Environment]::SetEnvironmentVariable("sharp_libvips_binary_host", 'https://cdn.npmmirror.com/binaries/sharp-libvips', 'User')
+    # [System.Environment]::SetEnvironmentVariable("CHROMEDRIVER_CDNURL", 'https://cdn.npmmirror.com/binaries/chromedriver', 'User')
+    # [System.Environment]::SetEnvironmentVariable("COREPACK_NPM_REGISTRY", 'https://registry.npmmirror.com', 'User')
+    # [System.Environment]::SetEnvironmentVariable("CYPRESS_DOWNLOAD_PATH_TEMPLATE", 'https://cdn.npmmirror.com/binaries/cypress/${version}/${platform}-${arch}/cypress.zip', 'User')
+    # [System.Environment]::SetEnvironmentVariable("EDGEDRIVER_CDNURL", 'https://npmmirror.com/mirrors/edgedriver', 'User')
+    # [System.Environment]::SetEnvironmentVariable("ELECTRON_BUILDER_BINARIES_MIRROR", 'https://cdn.npmmirror.com/binaries/electron-builder-binaries/', 'User')
+    # [System.Environment]::SetEnvironmentVariable("ELECTRON_MIRROR", 'https://cdn.npmmirror.com/binaries/electron/', 'User')
+    # [System.Environment]::SetEnvironmentVariable("NODEJS_ORG_MIRROR", 'https://cdn.npmmirror.com/binaries/node', 'User')
+    # [System.Environment]::SetEnvironmentVariable("NVM_NODEJS_ORG_MIRROR", 'https://cdn.npmmirror.com/binaries/node', 'User')
+    # [System.Environment]::SetEnvironmentVariable("NWJS_URLBASE", 'https://cdn.npmmirror.com/binaries/nwjs/v', 'User')
+    # [System.Environment]::SetEnvironmentVariable("OPERADRIVER_CDNURL", 'https://cdn.npmmirror.com/binaries/operadriver', 'User')
+    # [System.Environment]::SetEnvironmentVariable("PHANTOMJS_CDNURL", 'https://cdn.npmmirror.com/binaries/phantomjs', 'User')
+    # [System.Environment]::SetEnvironmentVariable("PLAYWRIGHT_DOWNLOAD_HOST", 'https://cdn.npmmirror.com/binaries/playwright', 'User')
+    # [System.Environment]::SetEnvironmentVariable("PRISMA_ENGINES_MIRROR", 'https://cdn.npmmirror.com/binaries/prisma', 'User')
+    # [System.Environment]::SetEnvironmentVariable("PUPPETEER_CHROME_DOWNLOAD_BASE_URL", 'https://cdn.npmmirror.com/binaries/chrome-for-testing', 'User')
+    # [System.Environment]::SetEnvironmentVariable("PUPPETEER_CHROME_HEADLESS_SHELL_DOWNLOAD_BASE_URL", 'https://cdn.npmmirror.com/binaries/chrome-for-testing', 'User')
+    # [System.Environment]::SetEnvironmentVariable("PUPPETEER_DOWNLOAD_BASE_URL", 'https://cdn.npmmirror.com/binaries/chrome-for-testing', 'User')
+    # [System.Environment]::SetEnvironmentVariable("PUPPETEER_DOWNLOAD_HOST", 'https://cdn.npmmirror.com/binaries/chrome-for-testing', 'User')
+    # [System.Environment]::SetEnvironmentVariable("RE2_DOWNLOAD_MIRROR", 'https://cdn.npmmirror.com/binaries/node-re2', 'User')
+    # [System.Environment]::SetEnvironmentVariable("RE2_DOWNLOAD_SKIP_PATH", 'true', 'User')
+    # [System.Environment]::SetEnvironmentVariable("SASS_BINARY_SITE", 'https://cdn.npmmirror.com/binaries/node-sass', 'User')
+    # [System.Environment]::SetEnvironmentVariable("SAUCECTL_INSTALL_BINARY_MIRROR", 'https://cdn.npmmirror.com/binaries/saucectl', 'User')
+    # [System.Environment]::SetEnvironmentVariable("SENTRYCLI_CDNURL", 'https://cdn.npmmirror.com/binaries/sentry-cli', 'User')
+    # [System.Environment]::SetEnvironmentVariable("SWC_BINARY_SITE", 'https://cdn.npmmirror.com/binaries/node-swc', 'User')
+    # # binary_host
+    # [System.Environment]::SetEnvironmentVariable("better_sqlite3_binary_host", 'https://cdn.npmmirror.com/binaries/better-sqlite3', 'User')
+    # [System.Environment]::SetEnvironmentVariable("gl_binary_host", 'https://cdn.npmmirror.com/binaries/gl', 'User')
+    # [System.Environment]::SetEnvironmentVariable("keytar_binary_host", 'https://cdn.npmmirror.com/binaries/keytar', 'User')
+    # [System.Environment]::SetEnvironmentVariable("robotjs_binary_host", 'https://cdn.npmmirror.com/binaries/robotjs', 'User')
+    # [System.Environment]::SetEnvironmentVariable("sharp_binary_host", 'https://cdn.npmmirror.com/binaries/sharp', 'User')
+    # [System.Environment]::SetEnvironmentVariable("sharp_libvips_binary_host", 'https://cdn.npmmirror.com/binaries/sharp-libvips', 'User')
 }
 
 if ($ConfigAction -eq "RESET") {
     Write-Host "Reset npm registry (npmjs.org)..." -ForegroundColor Blue
-    npm config set registry https://registry.npmjs.org/
+    npm config set registry "https://registry.npmjs.org/"
 
-    npm config delete disturl
-    npm config delete sass_binary_site
-    npm config delete electron_mirror
-    npm config delete puppeteer_download_base_url
-    npm config delete chromedriver_cdnurl
-    npm config delete operadriver_cdnurl
-    npm config delete phantomjs_cdnurl
-    npm config delete selenium_cdnurl
-    npm config delete node_inspector_cdnurl
+    # npm config delete disturl
+    # npm config delete sass_binary_site
+    # npm config delete electron_mirror
+    # npm config delete puppeteer_download_base_url
+    # npm config delete chromedriver_cdnurl
+    # npm config delete operadriver_cdnurl
+    # npm config delete phantomjs_cdnurl
+    # npm config delete selenium_cdnurl
+    # npm config delete node_inspector_cdnurl
 
     [System.Environment]::SetEnvironmentVariable("FNM_NODE_DIST_MIRROR", [NullString]::Value, 'User')
 
-    [System.Environment]::SetEnvironmentVariable("CHROMEDRIVER_CDNURL", [NullString]::Value, 'User')
-    [System.Environment]::SetEnvironmentVariable("COREPACK_NPM_REGISTRY", [NullString]::Value, 'User')
-    [System.Environment]::SetEnvironmentVariable("CYPRESS_DOWNLOAD_PATH_TEMPLATE", [NullString]::Value, 'User')
-    [System.Environment]::SetEnvironmentVariable("EDGEDRIVER_CDNURL", [NullString]::Value, 'User')
-    [System.Environment]::SetEnvironmentVariable("ELECTRON_BUILDER_BINARIES_MIRROR", [NullString]::Value, 'User')
-    [System.Environment]::SetEnvironmentVariable("ELECTRON_MIRROR", [NullString]::Value, 'User')
-    [System.Environment]::SetEnvironmentVariable("NODEJS_ORG_MIRROR", [NullString]::Value, 'User')
-    [System.Environment]::SetEnvironmentVariable("NVM_NODEJS_ORG_MIRROR", [NullString]::Value, 'User')
-    [System.Environment]::SetEnvironmentVariable("NWJS_URLBASE", [NullString]::Value, 'User')
-    [System.Environment]::SetEnvironmentVariable("OPERADRIVER_CDNURL", [NullString]::Value, 'User')
-    [System.Environment]::SetEnvironmentVariable("PHANTOMJS_CDNURL", [NullString]::Value, 'User')
-    [System.Environment]::SetEnvironmentVariable("PLAYWRIGHT_DOWNLOAD_HOST", [NullString]::Value, 'User')
-    [System.Environment]::SetEnvironmentVariable("PRISMA_ENGINES_MIRROR", [NullString]::Value, 'User')
-    [System.Environment]::SetEnvironmentVariable("PUPPETEER_CHROME_DOWNLOAD_BASE_URL", [NullString]::Value, 'User')
-    [System.Environment]::SetEnvironmentVariable("PUPPETEER_CHROME_HEADLESS_SHELL_DOWNLOAD_BASE_URL", [NullString]::Value, 'User')
-    [System.Environment]::SetEnvironmentVariable("PUPPETEER_DOWNLOAD_BASE_URL", [NullString]::Value, 'User')
-    [System.Environment]::SetEnvironmentVariable("PUPPETEER_DOWNLOAD_HOST", [NullString]::Value, 'User')
-    [System.Environment]::SetEnvironmentVariable("RE2_DOWNLOAD_MIRROR", [NullString]::Value, 'User')
-    [System.Environment]::SetEnvironmentVariable("RE2_DOWNLOAD_SKIP_PATH", [NullString]::Value, 'User')
-    [System.Environment]::SetEnvironmentVariable("SASS_BINARY_SITE", [NullString]::Value, 'User')
-    [System.Environment]::SetEnvironmentVariable("SAUCECTL_INSTALL_BINARY_MIRROR", [NullString]::Value, 'User')
-    [System.Environment]::SetEnvironmentVariable("SENTRYCLI_CDNURL", [NullString]::Value, 'User')
-    [System.Environment]::SetEnvironmentVariable("SWC_BINARY_SITE", [NullString]::Value, 'User')
-    # binary_host
-    [System.Environment]::SetEnvironmentVariable("better_sqlite3_binary_host", [NullString]::Value, 'User')
-    [System.Environment]::SetEnvironmentVariable("gl_binary_host", [NullString]::Value, 'User')
-    [System.Environment]::SetEnvironmentVariable("keytar_binary_host", [NullString]::Value, 'User')
-    [System.Environment]::SetEnvironmentVariable("robotjs_binary_host", [NullString]::Value, 'User')
-    [System.Environment]::SetEnvironmentVariable("sharp_binary_host", [NullString]::Value, 'User')
-    [System.Environment]::SetEnvironmentVariable("sharp_libvips_binary_host", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("CHROMEDRIVER_CDNURL", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("COREPACK_NPM_REGISTRY", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("CYPRESS_DOWNLOAD_PATH_TEMPLATE", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("EDGEDRIVER_CDNURL", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("ELECTRON_BUILDER_BINARIES_MIRROR", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("ELECTRON_MIRROR", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("NODEJS_ORG_MIRROR", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("NVM_NODEJS_ORG_MIRROR", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("NWJS_URLBASE", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("OPERADRIVER_CDNURL", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("PHANTOMJS_CDNURL", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("PLAYWRIGHT_DOWNLOAD_HOST", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("PRISMA_ENGINES_MIRROR", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("PUPPETEER_CHROME_DOWNLOAD_BASE_URL", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("PUPPETEER_CHROME_HEADLESS_SHELL_DOWNLOAD_BASE_URL", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("PUPPETEER_DOWNLOAD_BASE_URL", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("PUPPETEER_DOWNLOAD_HOST", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("RE2_DOWNLOAD_MIRROR", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("RE2_DOWNLOAD_SKIP_PATH", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("SASS_BINARY_SITE", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("SAUCECTL_INSTALL_BINARY_MIRROR", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("SENTRYCLI_CDNURL", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("SWC_BINARY_SITE", [NullString]::Value, 'User')
+    # # binary_host
+    # [System.Environment]::SetEnvironmentVariable("better_sqlite3_binary_host", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("gl_binary_host", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("keytar_binary_host", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("robotjs_binary_host", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("sharp_binary_host", [NullString]::Value, 'User')
+    # [System.Environment]::SetEnvironmentVariable("sharp_libvips_binary_host", [NullString]::Value, 'User')
 }
 
 npm config list
