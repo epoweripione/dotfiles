4dd591b013a8dc9dcb0157d39b09436f391545c8
epoweripione <siyuwuxin@gmail.com>
2026-04-20 16:37:59 +0800

    feat: ✨ init

---
+++ installer/sqlfluff_installer.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+trap 'rm -rf "${WORKDIR}"' EXIT
+
+[[ -z "${WORKDIR}" || "${WORKDIR}" != "/tmp/"* || ! -d "${WORKDIR}" ]] && WORKDIR="$(mktemp -d)"
+[[ -z "${CURRENT_DIR}" || ! -d "${CURRENT_DIR}" ]] && CURRENT_DIR=$(pwd)
+
+# Load custom functions
+if type 'colorEcho' 2>/dev/null | grep -q 'function'; then
+    :
+else
+    if [[ -s "${MY_SHELL_SCRIPTS:-$HOME/.dotfiles}/custom_functions.sh" ]]; then
+        source "${MY_SHELL_SCRIPTS:-$HOME/.dotfiles}/custom_functions.sh"
+    else
+        echo "${MY_SHELL_SCRIPTS:-$HOME/.dotfiles}/custom_functions.sh does not exist!"
+        exit 0
+    fi
+fi
+
+App_Installer_Reset
+
+# [SQLFluff - The SQL Linter for Humans](https://github.com/sqlfluff/sqlfluff)
+INSTALLER_APP_NAME="sqlfluff"
+INSTALLER_INSTALL_NAME="sqlfluff"
+
+PIP_PACKAGE_NAME="sqlfluff"
+
+if [[ -x "$(command -v ${INSTALLER_INSTALL_NAME})" ]]; then
+    INSTALLER_IS_UPDATE="yes"
+    INSTALLER_IS_INSTALL="no"
+else
+    [[ "${IS_UPDATE_ONLY}" == "yes" ]] && INSTALLER_IS_INSTALL="no"
+fi
+
+## Python
+# [[ -s "${MY_SHELL_SCRIPTS}/installer/python_pip_config.sh" ]] && source "${MY_SHELL_SCRIPTS}/installer/python_pip_config.sh"
+
+[[ ! -x "$(command -v pip)" ]] && INSTALLER_IS_INSTALL="no"
+
+[[ "${INSTALLER_IS_INSTALL}" == "yes" ]] && pip_Package_Install "${PIP_PACKAGE_NAME}"
+[[ "${INSTALLER_IS_UPDATE}" == "yes" ]] && pip install --upgrade "${INSTALLER_INSTALL_NAME}" >/dev/null 2>&1
