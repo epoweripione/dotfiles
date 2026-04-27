e5be2445e3e549c5507a364768362ef62f23d7cd
epoweripione <siyuwuxin@gmail.com>
2026-04-27 10:12:27 +0800

    refactor: 🌀 rewritten

--- manjaro/lyx_installer.sh
+++ manjaro/lyx_installer.sh
@@ -17,27 +17,29 @@ else
     fi
 fi
 
-[[ -z "${THE_WORLD_BLOCKED}" ]] && set_proxy_mirrors_env
-
-if [[ "${THE_WORLD_BLOCKED}" == "true" ]]; then
-    [[ -z "${MIRROR_LYX_BASE}" ]] && MIRROR_LYX_BASE="https://mirrors.tuna.tsinghua.edu.cn/lyx/"
-fi
-
 # [LyX](https://www.lyx.org/Home)
 colorEcho "${BLUE}Installing ${FUCHSIA}LyX${BLUE}..."
-if [[ -n "${MIRROR_LYX_BASE}" ]]; then
-    cd "${WORKDIR}" && yay -G lyx
-    if [[ -s "${WORKDIR}/lyx/PKGBUILD" ]]; then
-        # sed "s|https://ftp.lip6.fr/pub/lyx/|${MIRROR_LYX_BASE}|g" "${WORKDIR}/lyx/PKGBUILD" > "${WORKDIR}/lyx/PKGBUILD.new"
-        # diff -u "${WORKDIR}/lyx/PKGBUILD" "${WORKDIR}/lyx/PKGBUILD.new" > "${WORKDIR}/lyx/PKGBUILD.patch"
-        # patch -u "${WORKDIR}/lyx/PKGBUILD" -i "${WORKDIR}/lyx/PKGBUILD.patch"
-        sed -i "s|https://ftp.lip6.fr/pub/lyx/|${MIRROR_LYX_BASE}|g" "${WORKDIR}/lyx/PKGBUILD"
-        # updpkgsums
-        # yay -S lyx --answerdiff=None --noremovemake --pgpfetch --answerclean=None --noconfirm --asdeps
-        makepkg -si
-    fi
-else
-    yay --noconfirm --needed -S lyx
-fi
+yay --noconfirm --needed -S lyx
+
+# [[ -z "${THE_WORLD_BLOCKED}" ]] && set_proxy_mirrors_env
+
+# if [[ "${THE_WORLD_BLOCKED}" == "true" ]]; then
+#     [[ -z "${MIRROR_LYX_BASE}" ]] && MIRROR_LYX_BASE="https://mirrors.tuna.tsinghua.edu.cn/lyx/"
+# fi
+
+# if [[ -n "${MIRROR_LYX_BASE}" ]]; then
+#     cd "${WORKDIR}" && yay -G lyx
+#     if [[ -s "${WORKDIR}/lyx/PKGBUILD" ]]; then
+#         # sed "s|https://ftp.lip6.fr/pub/lyx/|${MIRROR_LYX_BASE}|g" "${WORKDIR}/lyx/PKGBUILD" > "${WORKDIR}/lyx/PKGBUILD.new"
+#         # diff -u "${WORKDIR}/lyx/PKGBUILD" "${WORKDIR}/lyx/PKGBUILD.new" > "${WORKDIR}/lyx/PKGBUILD.patch"
+#         # patch -u "${WORKDIR}/lyx/PKGBUILD" -i "${WORKDIR}/lyx/PKGBUILD.patch"
+#         sed -i "s|https://ftp.lip6.fr/pub/lyx/|${MIRROR_LYX_BASE}|g" "${WORKDIR}/lyx/PKGBUILD"
+#         # updpkgsums
+#         # yay -S lyx --answerdiff=None --noremovemake --pgpfetch --answerclean=None --noconfirm --asdeps
+#         cd "${WORKDIR}/lyx" && makepkg -si
+#     fi
+# else
+#     yay --noconfirm --needed -S lyx
+# fi
 
 cd "${CURRENT_DIR}" || exit
