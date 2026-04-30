2c15029fa223a3cf8a49b4d1a5d8ca997b822124
epoweripione <siyuwuxin@gmail.com>
2026-04-30 15:26:47 +0800

    feat: ✨ pnpm approve all pending builds without interactive prompts

--- functions/versions.sh
+++ functions/versions.sh
@@ -482,7 +482,7 @@ function npm_Global_Upgrade() {
     if [[ -x "$(command -v pnpm)" ]]; then
         colorEcho "${BLUE}Updating ${FUCHSIA}pnpm global packages${BLUE}..."
         pnpm update --global
-        pnpm approve-builds --global
+        pnpm approve-builds --global --all
     fi
 }
 
