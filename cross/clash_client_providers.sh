ded75b1cf194322ae7e486ee38b66b3ce6160d4c
epoweripione <siyuwuxin@gmail.com>
2026-06-27 15:12:25 +0800

    fix: 🐛 remove trailing whitespace in public-key, short-id

--- cross/clash_client_providers.sh
+++ cross/clash_client_providers.sh
@@ -331,8 +331,8 @@ function formatYAMLFile() {
     sed -ri 's/public-key:\s+([^,"\{\}]+)/public-key: "\1"/g' "${subscribeFile}"
     sed -ri 's/short-id:\s+([^,"\{\}]+)/short-id: "\1"/g' "${subscribeFile}"
 
-    sed -ri 's/public-key:\s+"(\S+)\s+"/public-key: "\1"/g' "${subscribeFile}"
-    sed -ri 's/short-id:\s+"(\S+)\s+"/short-id: "\1"/g' "${subscribeFile}"
+    sed -ri 's/public-key:\s+"?([^,\{\}[:space:]]+)[[:space:]]+"?/public-key: "\1"/g' "${subscribeFile}"
+    sed -ri 's/short-id:\s+"?([^,\{\}[:space:]]+)[[:space:]]+"?/short-id: "\1"/g' "${subscribeFile}"
 
     # replace characters not conform
     sed -ri -e 's/":"/:/g' -e 's/"\]:\["/]:[/g' -e 's/"\s+"/"/g' "${subscribeFile}"
