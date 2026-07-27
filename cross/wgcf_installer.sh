93f0586f5c0cc60b499c87b9ce86cc3cb585dc7d
epoweripione <siyuwuxin@gmail.com>
2026-07-27 19:27:53 +0800

    feat: ✨ add wgcf

---
+++ cross/wgcf_installer.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+trap 'rm -rf "${WORKDIR}"' EXIT
+
+[[ -z "${WORKDIR}" || "${WORKDIR}" != "/tmp/"* || ! -d "${WORKDIR}" ]] && WORKDIR="$(mktemp -d)"
+[[ -z "${CURRENT_DIR}" || ! -d "${CURRENT_DIR}" ]] && CURRENT_DIR=$(pwd)
+
+# Load custom functions
+if type 'colorEcho' 2>/dev/null | grep -q 'function'; then
+    :
+else
+    if [[ -s "${MY_SHELL_SCRIPTS:-$HOME/.dotfiles}/custom_functions.sh" ]]; then
+        source "${MY_SHELL_SCRIPTS:-$HOME/.dotfiles}/custom_functions.sh"
+    else
+        echo "${MY_SHELL_SCRIPTS:-$HOME/.dotfiles}/custom_functions.sh does not exist!"
+        exit 0
+    fi
+fi
+
+App_Installer_Reset
+
+# [wgcf is an unofficial, cross-platform CLI for Cloudflare Warp](https://github.com/ViRb3/wgcf)
+INSTALLER_GITHUB_REPO="ViRb3/wgcf"
+INSTALLER_BINARY_NAME="wgcf"
+INSTALLER_MATCH_PATTERN="wgcf_*"
+
+INSTALLER_VERSION_TO_FILE="yes"
+
+if [[ -x "$(command -v ${INSTALLER_BINARY_NAME})" ]]; then
+    INSTALLER_IS_UPDATE="yes"
+    App_Installer_Get_Installed_Version "${INSTALLER_BINARY_NAME}"
+else
+    [[ "${IS_UPDATE_ONLY}" == "yes" ]] && INSTALLER_IS_INSTALL="no"
+fi
+
+if [[ "${INSTALLER_IS_INSTALL}" == "yes" ]]; then
+    colorEcho "${BLUE}Checking ${FUCHSIA}${INSTALLER_BINARY_NAME}${BLUE}..."
+
+    INSTALLER_CHECK_URL="https://api.github.com/repos/${INSTALLER_GITHUB_REPO}/releases/latest"
+    App_Installer_Get_Remote_Version "${INSTALLER_CHECK_URL}"
+    if version_le "${INSTALLER_VER_REMOTE}" "${INSTALLER_VER_CURRENT}"; then
+        INSTALLER_IS_INSTALL="no"
+    fi
+fi
+
+if [[ "${INSTALLER_IS_INSTALL}" == "yes" ]]; then
+    installPrebuiltBinary "${INSTALLER_BINARY_NAME}" "${INSTALLER_GITHUB_REPO}" "${INSTALLER_MATCH_PATTERN}"
+fi
+
+## Usage
+# wgcf.exe register
+# wgcf.exe generate
+
+## cf account
+# wgcf-account.toml
+
+## WireGuard connetcion profile
+# wgcf-profile.conf
+
